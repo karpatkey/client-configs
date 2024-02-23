429d24f847f573765a3931ff4e4ceaa9b0b91703
Jan-Felix <jfschwarz@users.noreply.github.com>
2024-02-23 11:56:24 +0100

    use human-readable roleKey

--- clients/ens/roles/harvest/index.ts
+++ clients/ens/roles/harvest/index.ts
@@ -3,6 +3,6 @@ import { formatBytes32String } from "ethers/lib/utils"
 
 // export { permissions };
 
-export const roleKey = formatBytes32String("harvest")
+export const roleKey = "harvest"
 
 export const members = ["0x14c2d2d64c4860acf7cf39068eb467d7556197de"]
--- clients/ens/roles/swap/index.ts
+++ clients/ens/roles/swap/index.ts
@@ -1,5 +1,3 @@
-import { formatBytes32String } from "ethers/lib/utils"
-
-export const roleKey = formatBytes32String("swap")
+export const roleKey = "swap"
 
 export const members = ["0x14c2d2d64c4860acf7cf39068eb467d7556197de"]
