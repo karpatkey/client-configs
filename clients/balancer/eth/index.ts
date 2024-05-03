4a51c4982363268479b89d0014e90429fe780ac9
nledo <nicolas@karpatkey.com>
2024-05-03 14:13:32 -0300

    Balancer new roles v2

--- clients/balancer/eth/index.ts
+++ clients/balancer/eth/index.ts
@@ -2,7 +2,9 @@
 // Test-Safe: 0xC01318baB7ee1f5ba734172bF7718b5DC6Ec90E1
 // export const avatar = "0xC01318baB7ee1f5ba734172bF7718b5DC6Ec90E1"
 
-export const rolesMod = "0xBd1099dFD3c11b65FB4BB19A350da2f5B61Efb0d" // Test-Safe Roles Mod
+// Test-Safe Roles Mod v2: 0xBd1099dFD3c11b65FB4BB19A350da2f5B61Efb0d
+// Balancer Roles Mod v2: 0x13c61a25DB73e7a94a244bD2205aDba8b4a60F4a
+export const rolesMod = "0x13c61a25DB73e7a94a244bD2205aDba8b4a60F4a"
 
 export const chainId = 1
 
--- clients/balancer/eth/roles/manage/index.ts
+++ clients/balancer/eth/roles/manage/index.ts
@@ -2,6 +2,6 @@ import permissions from "./permissions_actions"
 
 export { permissions }
 
-export const roleKey = "BALANCER-MANAGER"
+export const roleKey = "MANAGER" // "BALANCER-MANAGER"
 
 export const members = ["0x60716991aCDA9E990bFB3b1224f1f0fB81538267"]
