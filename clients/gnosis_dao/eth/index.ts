067a3560fea0fc1d030b305bb308e059cffe0540
nledo <nicolas@karpatkey.com>
2024-06-07 11:55:40 -0300

    LTD Manager PP finished / Started DAO Manager PP

---
+++ clients/gnosis_dao/eth/index.ts
@@ -0,0 +1,7 @@
+export const avatar = "0x849D52316331967b6fF1198e5E32A0eB168D039d"
+
+export const rolesMod = "0x1cFB0CD7B1111bf2054615C7C491a15C4A3303cc"
+
+export const chainId = 1
+
+export * as roles from "./roles"
---
+++ clients/gnosis_dao/eth/roles/manage/index.ts
@@ -0,0 +1,7 @@
+import permissions from "./permissions"
+
+export { permissions }
+
+export const roleKey = "manage"
+
+export const members = ["0xf099e0f6604bde0aa860b39f7da75770b34ac804"]
