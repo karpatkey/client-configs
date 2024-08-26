5de6e5caabf306746ae61fbf5e993876c96b7bd3
nledo <nicolas@karpatkey.com>
2024-08-26 17:26:24 -0300

    Update dependencies / GnosisDAO config

--- clients/gnosis_dao/eth/index.ts
+++ clients/gnosis_dao/eth/index.ts
@@ -1,6 +1,8 @@
 export const avatar = "0x849D52316331967b6fF1198e5E32A0eB168D039d"
 
-export const rolesMod = "0x1cFB0CD7B1111bf2054615C7C491a15C4A3303cc" // 0xBd1099dFD3c11b65FB4BB19A350da2f5B61Efb0d
+// Test-Safe Roles Mod v2: 0xBd1099dFD3c11b65FB4BB19A350da2f5B61Efb0d
+// GnosisDAO Roles Mod v2: 0x27d8bb2e33Bc38A9CE93fdD90C80677b8436aFfb
+export const rolesMod = "0xBd1099dFD3c11b65FB4BB19A350da2f5B61Efb0d"
 
 export const chainId = 1
 
