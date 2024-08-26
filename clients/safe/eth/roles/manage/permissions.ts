d7c87e17efe6234a0b6493d2b38b4a735feb8164
nledo <nicolas@karpatkey.com>
2024-08-26 18:13:38 -0300

    SAFE setup progress

--- clients/safe/eth/roles/manage/permissions.ts
+++ clients/safe/eth/roles/manage/permissions.ts
@@ -1,9 +1,7 @@
 import { c } from "zodiac-roles-sdk"
 import { allow } from "zodiac-roles-sdk/kit"
 import { allow as allowAction } from "defi-kit/eth"
-import {
-  SAFE,
-} from "../../../../../eth-sdk/addresses"
+import { SAFE } from "../../../../../eth-sdk/addresses"
 import { contracts } from "../../../../../eth-sdk/config"
 import { allowErc20Approve } from "../../../../../utils/erc20"
 import { PermissionList } from "../../../../../types"
@@ -12,7 +10,6 @@ export default [
   /*********************************************
    * Defi-Kit permissions
    *********************************************/
-
   /*********************************************
    * Typed-presets permissions
    *********************************************/
