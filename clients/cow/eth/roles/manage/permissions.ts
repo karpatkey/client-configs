3ca1cd1a8843b277363cfb0a73813ffe5739fcbe
nledo <nicolas@karpatkey.com>
2024-08-29 18:57:49 -0300

    Cow setup progress

---
+++ clients/cow/eth/roles/manage/permissions.ts
@@ -0,0 +1,16 @@
+import { c } from "zodiac-roles-sdk"
+import { allow } from "zodiac-roles-sdk/kit"
+import { allow as allowAction } from "defi-kit/eth"
+import { COW } from "../../../../../eth-sdk/addresses"
+import { contracts } from "../../../../../eth-sdk/config"
+import { allowErc20Approve } from "../../../../../utils/erc20"
+import { PermissionList } from "../../../../../types"
+
+export default [
+  /*********************************************
+   * Defi-Kit permissions
+   *********************************************/
+  /*********************************************
+   * Typed-presets permissions
+   *********************************************/
+]
