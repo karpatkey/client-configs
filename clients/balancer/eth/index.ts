fe2583b9658008644777ea544b8572492dd156c2
nledo <nicolas@karpatkey.com>
2024-09-23 16:20:30 -0300

    Update Manager Policy

--- clients/balancer/eth/index.ts
+++ clients/balancer/eth/index.ts
@@ -4,7 +4,7 @@ export const avatar = "0x0EFcCBb9E2C09Ea29551879bd9Da32362b32fc89"
 
 // Test-Safe Roles Mod v2: 0xBd1099dFD3c11b65FB4BB19A350da2f5B61Efb0d
 // Balancer Roles Mod v2: 0x13c61a25DB73e7a94a244bD2205aDba8b4a60F4a
-export const rolesMod = "0xBd1099dFD3c11b65FB4BB19A350da2f5B61Efb0d"
+export const rolesMod = "0x13c61a25DB73e7a94a244bD2205aDba8b4a60F4a"
 
 export const chainId = 1
 
