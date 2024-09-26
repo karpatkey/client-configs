d3f92c8b3be4ae8d20570b6ff0bcbad3f453e17f
nledo <nicolas@karpatkey.com>
2024-09-26 16:00:59 -0300

    Progress in MANAGER policy

--- clients/gnosis_dao/gno/roles/manage/permissions.ts
+++ clients/gnosis_dao/gno/roles/manage/permissions.ts
@@ -1,6 +1,6 @@
 import { c } from "zodiac-roles-sdk"
 import { allow } from "zodiac-roles-sdk/kit"
-import { allow as allowAction } from "defi-kit/eth"
+import { allow as allowAction } from "defi-kit/gno"
 import {
   WXDAI,
   sDAI,
@@ -17,6 +17,26 @@ export default [
   /*********************************************
    * Defi-Kit permissions
    *********************************************/
+  // Aave v3 - Deposit XDAI
+  allowAction.aave_v3.deposit({ targets: ["XDAI"] }),
+  // Aave v3 - Deposit WXDAI
+  allowAction.aave_v3.deposit({ targets: ["WXDAI"] }),
+  // Aave v3 - Deposit wstETH
+  allowAction.aave_v3.deposit({ targets: ["wstETH"] }),
+  // Aave v3 - Deposit WETH
+  allowAction.aave_v3.deposit({ targets: ["WETH"] }),
+  // Aave v3 - Deposit USDC
+  allowAction.aave_v3.deposit({ targets: ["USDC"] }),
+  // Aave v3 - Deposit EURe
+  allowAction.aave_v3.deposit({ targets: ["EURe"] }),
+  // Aave v3 - Deposit GNO
+  allowAction.aave_v3.deposit({ targets: ["GNO"] }),
+  // Aave v3 - Deposit USDC.e
+  allowAction.aave_v3.deposit({ targets: ["USDC.e"] }),
+
+
+
+
   /*********************************************
    * Typed-presets permissions
    *********************************************/
