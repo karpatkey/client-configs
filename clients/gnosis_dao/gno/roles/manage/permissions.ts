5e4ec4e17621033836600821185dd65f72a21d36
nledo <nicolas@karpatkey.com>
2024-09-27 17:55:48 -0300

    Progress in MANAGER policy

--- clients/gnosis_dao/gno/roles/manage/permissions.ts
+++ clients/gnosis_dao/gno/roles/manage/permissions.ts
@@ -34,6 +34,56 @@ export default [
   // Aave v3 - Deposit USDC.e
   allowAction.aave_v3.deposit({ targets: ["USDC.e"] }),
 
+  // Aura - WETH/wstETH
+  allowAction.aura.deposit({ targets: ["0"] }),
+  // Aura - EURe/sDAI
+  allowAction.aura.deposit({ targets: ["18"] }),
+  // Aura - Gyroscope ECLP rETH/WETH
+  allowAction.aura.deposit({ targets: ["26"] }),
+
+  // Balancer - wstETH/GNO
+  allowAction.balancer.deposit({ targets: ["B-50wstETH-50GNO"] }),
+  allowAction.balancer.stake({ targets: ["B-50wstETH-50GNO"] }),
+  // Balancer - sDAI/wstETH
+  allowAction.balancer.deposit({ targets: ["B-50sDAI-50wstETH"] }),
+  allowAction.balancer.stake({ targets: ["B-50sDAI-50wstETH"] }),
+  // Balancer - USDC.e/USDT/sDAI
+  allowAction.balancer.deposit({ targets: ["sBAL3"] }),
+  allowAction.balancer.stake({ targets: ["sBAL3"] }),
+  // Balancer - WETH/wstETH
+  allowAction.balancer.deposit({ targets: ["bb-WETH-wstETH"] }),
+  allowAction.balancer.stake({ targets: ["bb-WETH-wstETH"] }),
+  // Balancer - rETH/WETH
+  allowAction.balancer.deposit({ targets: ["ECLP-rETH-WETH"] }),
+  allowAction.balancer.stake({ targets: ["ECLP-rETH-WETH"] }),
+  // Balancer - osGNO/GNO
+  allowAction.balancer.deposit({ targets: ["osGNO/GNO-BPT"] }),
+  allowAction.balancer.stake({ targets: ["osGNO/GNO-BPT"] }),
+  // Balancer - WBTC/WETH
+  allowAction.balancer.deposit({ targets: ["50WBTC-50WETH"] }),
+  allowAction.balancer.stake({ targets: ["50WBTC-50WETH"] }),
+  // Balancer - Gyroscope ECLP wstETH/WETH
+  allowAction.balancer.deposit({ targets: ["ECLP-wstETH-WETH"] }),
+  allowAction.balancer.stake({ targets: ["ECLP-wstETH-WETH"] }),
+  // Balancer - wstETH/BAL/AURA
+  allowAction.balancer.deposit({ targets: ["B-50wstETH-25BAL-25AURA"] }),
+  allowAction.balancer.stake({ targets: ["B-50wstETH-25BAL-25AURA"] }),
+  // Balancer - wstETH/COW
+  allowAction.balancer.deposit({ targets: ["B-50wstETH-50COW"] }),
+  allowAction.balancer.stake({ targets: ["B-50wstETH-50COW"] }),
+  // Balancer - COW/GNO
+  allowAction.balancer.deposit({ targets: ["50COW-50GNO"] }),
+  allowAction.balancer.stake({ targets: ["50COW-50GNO"] }),
+  // Balancer - stEUR/EURe
+  allowAction.balancer.deposit({ targets: ["stEUR/EURe"] }),
+  allowAction.balancer.stake({ targets: ["stEUR/EURe"] }),
+  // Balancer - GIV/GNO
+  allowAction.balancer.deposit({ targets: ["50GIV-50GNO"] }),
+  allowAction.balancer.stake({ targets: ["50GIV-50GNO"] }),
+  // Balancer - WXDAI/GNO
+  allowAction.balancer.deposit({ targets: ["WXDAI-GNO"] }),
+  allowAction.balancer.stake({ targets: ["WXDAI-GNO"] }),
+
   /*********************************************
    * Typed-presets permissions
    *********************************************/
