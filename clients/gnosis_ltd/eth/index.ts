7a465aa901e793c66dc5cc788b522831ba2ff91f
nledo <nicolas@karpatkey.com>
2024-10-03 15:49:38 -0300

    prettier

--- clients/gnosis_ltd/eth/index.ts
+++ clients/gnosis_ltd/eth/index.ts
@@ -2,7 +2,7 @@ export const avatar = "0x4971DD016127F390a3EF6b956Ff944d0E2e1e462"
 
 // Test-Safe Roles Mod v2: 0xBd1099dFD3c11b65FB4BB19A350da2f5B61Efb0d
 // GnosisLTD Roles Mod v2: 0x2b818761ebCf1CcA5e609cC71D8feCB658ADBa20
-export const rolesMod = "0xBd1099dFD3c11b65FB4BB19A350da2f5B61Efb0d"
+export const rolesMod = "0x2b818761ebCf1CcA5e609cC71D8feCB658ADBa20"
 
 export const chainId = 1
 
