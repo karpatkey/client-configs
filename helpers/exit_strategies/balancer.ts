e5f01d8f77d01dbc9e29d85023db9e8ff6478227
nledo <nicolas@karpatkey.com>
2024-10-09 09:47:33 -0300

    Helper functions migration

--- helpers/exit_strategies/balancer.ts
+++ helpers/exit_strategies/balancer.ts
@@ -3,8 +3,8 @@ import { allow } from "zodiac-roles-sdk/kit"
 import { Address } from "@dethcrypto/eth-sdk"
 import { sdks } from "../sdks"
 import { ChainId } from "../../types"
-import { ethers } from "ethers"
-import { providers } from "../providers"
+// import { ethers } from "ethers"
+// import { providers } from "../providers"
 
 export const balancer__withdraw = (balancerPoolId: string): PermissionSet => {
   return [
@@ -18,25 +18,31 @@ export const balancer__unstake_withdraw = async (
   gauge: Address
 ): Promise<PermissionSet> => {
   const sdk = sdks[chainId]
-  const provider = providers[chainId]
 
-  const gaugeContract = new ethers.Contract(
-    gauge,
-    sdk.balancer.gauge.interface.format(ethers.utils.FormatTypes.json),
-    provider
-  )
+  const bpt = await sdk.balancer.gauge.attach(gauge).lp_token()
+  const balancerPoolId = await sdk.balancer.bpt.attach(bpt).getPoolId()
 
-  const bpt = await gaugeContract.lp_token()
+  // Another way to do the same without the attach()
+  // const provider = providers[chainId]
 
-  const bptContract = new ethers.Contract(
-    bpt,
-    sdk.balancer.bpt.interface.format(ethers.utils.FormatTypes.json),
-    provider
-  )
+  // const gaugeContract = new ethers.Contract(
+  //   gauge,
+  //   sdk.balancer.gauge.interface.format(ethers.utils.FormatTypes.json),
+  //   provider
+  // )
 
-  const balancerPoolId = await bptContract.getPoolId()
+  // const bpt = await gaugeContract.lp_token()
+
+  // const bptContract = new ethers.Contract(
+  //   bpt,
+  //   sdk.balancer.bpt.interface.format(ethers.utils.FormatTypes.json),
+  //   provider
+  // )
+
+  // const balancerPoolId = await bptContract.getPoolId()
 
   return [
+    // It doesn't matter the blockchain we use, since we are overwriting
     // the address of the rewarder (abis are the same indistinctively of the blockchain)
     {
       ...allow.mainnet.balancer.gauge["withdraw(uint256)"](),
