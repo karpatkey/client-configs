6b09bcf72c901eccdf670191d049b6836c2c92e2
nledo <nicolas@karpatkey.com>
2024-10-10 13:30:23 -0300

    Progress with MANAGER policy

--- clients/gnosis_ltd/gno/index.ts
+++ clients/gnosis_ltd/gno/index.ts
@@ -1,8 +1,8 @@
 export const avatar = "0x10E4597fF93cbee194F4879f8f1d54a370DB6969"
 
 // Test-Safe Roles Mod v2: 0xBd1099dFD3c11b65FB4BB19A350da2f5B61Efb0d
-// GnosisLTD Roles Mod v2:
-export const rolesMod = ""
+// GnosisLTD Roles Mod v2: 0xB99217F1CE1e511DBA7B5d39c3589ae479f97a83
+export const rolesMod = "0xB99217F1CE1e511DBA7B5d39c3589ae479f97a83"
 
 export const chainId = 100
 
--- clients/gnosis_ltd/gno/roles/manage/permissions.ts
+++ clients/gnosis_ltd/gno/roles/manage/permissions.ts
@@ -1,4 +1,3 @@
-import { c } from "zodiac-roles-sdk"
 import { allow } from "zodiac-roles-sdk/kit"
 import { allow as allowAction } from "defi-kit/gno"
 import { contracts } from "../../../../../eth-sdk/config"
