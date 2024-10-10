93ec6d5cd6f7f21e5b9be2b348121995f0b0149b
Zarathustra <santiago@karpatkey.com>
2024-10-10 17:15:14 -0300

    feat: aura relocker policy

---
+++ clients/gnosis_dao/eth/roles/guardians/auraRelocker/permissions.ts
@@ -0,0 +1,21 @@
+import { allow } from "zodiac-roles-sdk/kit"
+import { PermissionList } from "../../../../../../types"
+
+export default [
+  allow.mainnet.aura.claim_zap.claimRewards(
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    {
+      claimCvxCrv: undefined,
+      claimLockedCvx: undefined,
+      lockCvxCrv: false,
+      lockCrvDeposit: false,
+      useAllWalletFunds: false,
+      useCompounder: false,
+      lockCvx: true,
+    }
+  ),
+] satisfies PermissionList
