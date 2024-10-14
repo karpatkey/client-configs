97e4eb46cbe73dcd2111035c4f698705ce5d5a91
nledo <nicolas@karpatkey.com>
2024-10-14 14:01:44 -0300

    prettier

--- clients/gnosis_dao/eth/roles/disassamble/permissions.ts
+++ clients/gnosis_dao/eth/roles/disassamble/permissions.ts
@@ -1,21 +1,13 @@
 import { c } from "zodiac-roles-sdk"
 import { allow } from "zodiac-roles-sdk/kit"
-import {
-  wstETH
-} from "../../../../../eth-sdk/addresses"
+import { wstETH } from "../../../../../eth-sdk/addresses"
 import { PermissionList } from "../../../../../types"
 
 export default [
   // Aave v2 - Unstake GHO from Safety Module
   allow.mainnet.aave_v2.stkGHO.cooldown(),
-  allow.mainnet.aave_v2.stkGHO.redeem(
-    c.avatar
-  ),
+  allow.mainnet.aave_v2.stkGHO.redeem(c.avatar),
 
   // Aave v3 - Withdraw wstETH
-  allow.mainnet.aave_v3.pool_v3.withdraw(
-    wstETH,
-    undefined,
-    c.avatar
-  )
+  allow.mainnet.aave_v3.pool_v3.withdraw(wstETH, undefined, c.avatar),
 ] satisfies PermissionList
