3d2352d8cecde03439419a19d32ddcf875188ce2
nledo <nicolas@karpatkey.com>
2024-10-18 12:05:47 -0300

    PUR#6: Balancer Manager Policy Update

--- clients/balancer/gno/roles/manage/index.ts
+++ clients/balancer/gno/roles/manage/index.ts
@@ -2,6 +2,6 @@ import permissions from "./permissions"
 
 export { permissions }
 
-export const roleKey = "MANAGER"
+export const roleKey = "MANAGER" // "BALANCER-MANAGER"
 
 export const members = ["0x60716991aCDA9E990bFB3b1224f1f0fB81538267"]
