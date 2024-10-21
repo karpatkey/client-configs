0beef0724c333642e129a1598478e0c9b5fcf8d9
am-ramona <ramona.abimoussa@gmail.com>
2024-10-21 17:12:37 +0300

    Finalized the Disassemble Policy

--- clients/gnosis_dao/eth/roles/disassamble/permissions.ts
+++ clients/gnosis_dao/eth/roles/disassamble/permissions.ts
@@ -1,13 +1,16 @@
 import { c } from "zodiac-roles-sdk"
 import { allow } from "zodiac-roles-sdk/kit"
 import {
+  ankrETH,
   DAI,
   GHO,
+  ETHx,
   rETH,
   USDC,
   USDT,
   WBTC,
   WETH,
+  stETH,
   wstETH,
   aura,
   balancer,
@@ -33,6 +36,8 @@ export default [
   /*********************************************
    * Protocol permissions
    *********************************************/
+
+                                                             
   // Aave v3 - Withdraw wstETH
   allow.mainnet.aave_v3.pool_v3.withdraw(wstETH, undefined, c.avatar),
 
@@ -45,8 +50,9 @@ export default [
   // Aave v3 - Repay WBTC
   allow.mainnet.aave_v3.pool_v3.repay(WBTC, undefined, undefined, c.avatar),
 
+
+                                                              
   // Aura - auraBAL
-  // we use aura & balancer addresses from addresses.ts with the helper functions And the ones in config.ts for the others
   aura__withdraw_balancer(
     aura.auraB_auraBAL_stable_rewarder,
     balancer.B_auraBAL_stable_pId
@@ -70,6 +76,8 @@ export default [
     balancer.B_rETH_stable_pid
   ),
 
+
+                                                              
   // Balancer - auraBAL / B-80BAL-20WETH
   balancer__unstake_withdraw(Chain.eth, balancer.B_auraBAL_STABLE_gauge),
   // Balancer - B-80BAL-20WETH
@@ -84,39 +92,65 @@ export default [
   // Balancer - rETH/WETH
   balancer__unstake_withdraw(Chain.eth, balancer.B_rETH_stable_gauge),
 
-  // Lido
+  
+  // Lido   
   lido__unstake_stETH(),
   lido__unwrap_and_unstake_wstETH(),
+
 
   /*********************************************
    * SWAPS
    *********************************************/
+
+                                                                          
   // Balancer - Swap rETH <-> WETH
   balancer__swap(balancer.B_rETH_stable_pid, [rETH, WETH], [rETH, WETH]),
 
   // Balancer - Swap WETH <-> wstETH
+  balancer__swap(balancer.B_stETH_stable_pid, [WETH, wstETH], [wstETH, WETH]),
 
+
+                                                                          
   // CowSwap - DAI -> [ETH, USDC, USDT]
   cowswap__swap([DAI], [E_ADDRESS, USDC, USDT], Chain.eth),
 
+  // CowSwap - USDT -> [USDC, DAI, E_ADDRESS]
+  cowswap__swap([USDT], [USDC, DAI, E_ADDRESS], Chain.eth),
+
+  // Cowswap - USDC -> [DAI, USDT, E_ADDRESS]
+  cowswap__swap([USDC], [DAI, USDT, E_ADDRESS], Chain.eth), 
+
+  // Cowswap - WETH -> [DAI, USDT, USDC]
+  cowswap__swap([WETH], [DAI, USDT, USDC], Chain.eth),
+
+
+                                                                          
   // Curve - Swaps in 3pool
   ...allowErc20Approve([DAI, USDC, USDT], [contracts.mainnet.curve.x3CRV_pool]),
   allow.mainnet.curve.x3CRV_pool["exchange"](),
 
   // Curve - Swap ETH/stETH (steCRV)
+  ...allowErc20Approve([stETH], [contracts.mainnet.curve.steth_eth_pool]),
+  allow.mainnet.curve.steth_eth_pool["exchange"](),
 
   // Curve - Swaps ETH/stETH (stETH-ng-f)
+  ...allowErc20Approve([stETH], [contracts.mainnet.curve.stETH_ng_f_pool]),
+  allow.mainnet.curve.stETH_ng_f_pool["exchange(int128,int128,uint256,uint256)"](),
 
+
+                                                                         
   // Uniswap V3 - Swaps
-  // Add approvals
+  ...allowErc20Approve([ankrETH, DAI, ETHx, USDC, USDT, WETH, wstETH], [contracts.mainnet.uniswap_v3.router_2]),
   allow.mainnet.uniswap_v3.router_2.exactInputSingle(
     {
-      tokenIn: c.or(USDC, USDT), // example, please change for the right tokens
-      tokenOut: c.or(USDC, USDT), // example, please change for the right tokens
+      tokenIn: c.or(ankrETH, DAI, ETHx, USDC, USDT, WETH, wstETH), 
+      tokenOut: c.or(DAI, USDC, USDT, WETH, wstETH), 
       recipient: c.avatar,
     },
     {
       send: true,
+
     }
   ),
+
 ] satisfies PermissionList
