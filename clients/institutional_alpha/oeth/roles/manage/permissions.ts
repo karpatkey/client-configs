6e15580b6c1e032aabdd51c03a37af35d94bbf06
nledo <nicolas@karpatkey.com>
2024-10-28 14:49:49 -0300

    prettier

--- clients/institutional_alpha/oeth/roles/manage/permissions.ts
+++ clients/institutional_alpha/oeth/roles/manage/permissions.ts
@@ -1,8 +1,6 @@
 import { c } from "zodiac-roles-sdk"
 import { allow } from "zodiac-roles-sdk/kit"
-import {
-  USDT
-} from "../../../../../eth-sdk/addresses_arb"
+import { USDT } from "../../../../../eth-sdk/addresses_arb"
 import { contracts } from "../../../../../eth-sdk/config"
 import { allowErc20Approve } from "../../../../../utils/erc20"
 import { PermissionList } from "../../../../../types"
