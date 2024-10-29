6e61ee01b3208ee6374aa12785d331f770b32b84
nledo <nicolas@karpatkey.com>
2024-10-29 16:54:41 -0300

    Add DISASSEMBLER and SWAPPER members to Institutional Stable Fund

--- clients/institutional/arb1/roles/swap/index.ts
+++ clients/institutional/arb1/roles/swap/index.ts
@@ -4,4 +4,4 @@ export { permissions }
 
 export const roleKey = "SWAPPER"
 
-export const members = [""]
+export const members = ["0x85A2dB015d3907b575A541A974540A1Cb0eC1083"]
--- clients/institutional/gno/roles/disassemble/index.ts
+++ clients/institutional/gno/roles/disassemble/index.ts
@@ -4,4 +4,4 @@ export { permissions }
 
 export const roleKey = "DISASSEMBLER"
 
-export const members = [""]
+export const members = ["0xB7462Dc7557FC081048A99b9A83C8d970da695DE"]
