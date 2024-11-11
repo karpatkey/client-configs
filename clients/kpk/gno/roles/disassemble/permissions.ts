beec6811e2583d0afef2fb30349acc27740ebc96
am-ramona <ramona.abimoussa@gmail.com>
2024-11-11 21:56:39 +0200

    Develop policy

---
+++ clients/kpk/gno/roles/disassemble/permissions.ts
@@ -0,0 +1,16 @@
+import { allow } from "zodiac-roles-sdk/kit"
+import { c } from "zodiac-roles-sdk"
+import { PermissionList } from "../../../../../types"
+
+export default [
+  /*********************************************
+   * Protocol permissions
+   *********************************************/
+
+  // Unwrap of XDAI
+  allow.gnosis.wxdai.withdraw(),
+
+  // Spark - DSR/sDAI
+  allow.mainnet.spark.sDAI.redeem(undefined, c.avatar, c.avatar),
+  allow.mainnet.spark.sDAI.withdraw(undefined, c.avatar, c.avatar),
+] satisfies PermissionList
