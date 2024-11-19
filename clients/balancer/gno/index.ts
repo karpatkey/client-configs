44f5580bcb50d56c1a228877147616c759237efd
nledo <nicolas@karpatkey.com>
2024-11-19 16:55:52 -0300

    Balancers' disassemble and swapper policies on GC

--- clients/balancer/gno/index.ts
+++ clients/balancer/gno/index.ts
@@ -2,7 +2,7 @@
 // Test-Safe: 0xC01318baB7ee1f5ba734172bF7718b5DC6Ec90E1
 export const avatar = "0x0EFcCBb9E2C09Ea29551879bd9Da32362b32fc89"
 
-// Test-Safe Roles Mod v2: 0x9a1c0d35ecdB0622DA6dC2FAF373Ec61716ED5F5
+// Test-Safe Roles Mod v2: 0xBd1099dFD3c11b65FB4BB19A350da2f5B61Efb0d
 // Balancer Roles Mod v2: 0x13c61a25DB73e7a94a244bD2205aDba8b4a60F4a
 export const rolesMod = "0x13c61a25DB73e7a94a244bD2205aDba8b4a60F4a"
 
