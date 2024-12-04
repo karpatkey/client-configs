aa2d9819475a57c47b6fb20ce6dbd832cdeca89d
nledo <nicolas@karpatkey.com>
2024-12-04 16:23:36 -0300

    Onboard Institutional ETH LST Fund

---
+++ clients/institutional_eth/eth/roles/disassemble/permissions.ts
@@ -0,0 +1,72 @@
+import { c } from "zodiac-roles-sdk"
+import { allow } from "zodiac-roles-sdk/kit"
+import {
+  eETH,
+  weETH,
+  aura,
+  balancer,
+  convex,
+} from "../../../../../eth-sdk/addresses"
+import { contracts } from "../../../../../eth-sdk/config"
+import { allowErc20Approve } from "../../../../../utils/erc20"
+import { PermissionList } from "../../../../../types"
+import {
+  aura__withdraw_balancer,
+  balancer__unstake_withdraw,
+  convex__withdraw,
+} from "../../../../../helpers/exit_strategies"
+import { Chain } from "../../../../../types"
+
+export default [
+  // Unwrap ETH
+  allow.mainnet.weth.withdraw(),
+
+  // Aura - rETH/WETH
+  aura__withdraw_balancer(
+    aura.auraBrEthStableRewarder,
+    balancer.bREthStablePid
+  ),
+  // Aura - osETH/WETH
+  aura__withdraw_balancer(aura.auraosEthWethRewarder, balancer.osEthWethPid),
+
+  // Balancer - rETH/WETH
+  balancer__unstake_withdraw(Chain.eth, balancer.bREthStableGauge),
+  // Balancer - WETH/osETH
+  balancer__unstake_withdraw(Chain.eth, balancer.osEthWethGauge),
+
+  // Convex - ETHx/ETH - ethx-f
+  convex__withdraw(convex.cvxethxRewarder),
+  // Convex - ankrETH/ETH
+  convex__withdraw(convex.cvxankrCrvRewarder),
+
+  // Curve - ETHx/ETH - ethx-f
+  allow.mainnet.curve.ethxfPool["remove_liquidity(uint256,uint256[2])"](),
+  allow.mainnet.curve.ethxfPool[
+    "remove_liquidity_imbalance(uint256[2],uint256)"
+  ](),
+  allow.mainnet.curve.ethxfPool[
+    "remove_liquidity_one_coin(uint256,int128,uint256)"
+  ](),
+  allow.mainnet.curve.ethxfGauge["withdraw(uint256)"](),
+  // Curve - ankrETH/ETH
+  allow.mainnet.curve.ankrCrvPool.remove_liquidity(),
+  allow.mainnet.curve.ankrCrvPool.remove_liquidity_imbalance(),
+  allow.mainnet.curve.ankrCrvPool.remove_liquidity_one_coin(),
+  allow.mainnet.curve.ankrCrvGauge.withdraw(),
+
+  // ether.fi - Liquid ETH
+  ...allowErc20Approve(
+    [contracts.mainnet.etherfi.liquidEth],
+    [contracts.mainnet.etherfi.atomicQueue]
+  ),
+  allow.mainnet.etherfi.atomicQueue.updateAtomicRequest(
+    contracts.mainnet.etherfi.liquidEth,
+    c.or(eETH, weETH)
+  ),
+
+  // Uniswap v3 - WETH/wstETH - cbETH/WETH
+  allow.mainnet.uniswapV3.positionsNft.decreaseLiquidity(),
+  allow.mainnet.uniswapV3.positionsNft.collect({
+    recipient: c.avatar,
+  }),
+] satisfies PermissionList
