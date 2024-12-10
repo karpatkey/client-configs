9af76e11c89884402375317cc39442a9e46a5c5e
nledo <nicolas@karpatkey.com>
2024-12-10 16:57:27 -0300

    Gnosis DAO and LTD Roles Update / Bump roles-sdk and DeFi-Kit versions

--- clients/institutional_alpha/base/index.ts
+++ clients/institutional_alpha/base/index.ts
@@ -1,5 +1,7 @@
 export const avatar = "0xdF8EE91120154BdC3Cb628f0535B6511e52327Ff"
 
+// Test-Safe Roles Mod v2: 0xBd1099dFD3c11b65FB4BB19A350da2f5B61Efb0d
+// Institutional Alpha Fund Roles Mod v2: 0xa14ed1760Ab65474D1f28836A5B8489ff604c4a9
 export const rolesMod = "0xa14ed1760Ab65474D1f28836A5B8489ff604c4a9"
 
 export const chainId = 8453
