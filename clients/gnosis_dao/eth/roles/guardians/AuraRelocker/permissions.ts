2cfe16939fb21dfca3330f65f35fd69cc3e678c9
nledo <nicolas@karpatkey.com>
2024-12-23 17:00:21 -0300

    Tweak AuraRelocker Policy for Gnosis DAO on Mainnet

--- clients/gnosis_dao/eth/roles/guardians/AuraRelocker/permissions.ts
+++ clients/gnosis_dao/eth/roles/guardians/AuraRelocker/permissions.ts
@@ -1,7 +1,11 @@
 import { allow } from "zodiac-roles-sdk/kit"
+import { AURA } from "../../../../../../eth-sdk/addresses"
+import { contracts } from "../../../../../../eth-sdk/config"
+import { allowErc20Approve } from "../../../../../../utils/erc20"
 import { PermissionList } from "../../../../../../types"
 
 export default [
+  allowErc20Approve([AURA], [contracts.mainnet.aura.claimZapV3]),
   allow.mainnet.aura.claimZapV3.claimRewards(
     undefined,
     undefined,
