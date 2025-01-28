ea56245bd07270f728e4cd99c9664274141e722d
Jan-Felix <jfschwarz@users.noreply.github.com>
2025-01-28 17:33:47 +0100

    migrate safe

--- clients/safe/eth/roles/manage/permissions/_actions.ts
+++ clients/safe/eth/roles/manage/permissions/_actions.ts
@@ -1,5 +1,3 @@
-import { c } from "zodiac-roles-sdk"
-import { allow } from "zodiac-roles-sdk/kit"
 import { allow as allowAction } from "defi-kit/eth"
 import {
   AURA,
@@ -12,11 +10,7 @@ import {
   USDC,
   WETH,
   wstETH,
-  uniswapV2,
 } from "@/addresses/eth"
-import { contracts } from "@/contracts"
-import { allowErc20Approve } from "@/helpers"
-import { PermissionList } from "@/types"
 
 export default [
   /*********************************************
@@ -63,32 +57,4 @@ export default [
 
   // Uniswap v3 - SAFE + WETH - Current NFT Ids: 711659 and 774338
   allowAction.uniswap_v3.deposit({ tokens: ["SAFE", "WETH"] }),
-
-  /*********************************************
-   * Typed-presets permissions
-   *********************************************/
-  // Uniswap v2 - SAFE/WETH
-  allowErc20Approve([SAFE], [contracts.mainnet.uniswapV2.router2]),
-  allow.mainnet.uniswapV2.router2.addLiquidityETH(
-    SAFE,
-    undefined,
-    undefined,
-    undefined,
-    c.avatar,
-    undefined,
-    {
-      send: true,
-    }
-  ),
-  allowErc20Approve(
-    [uniswapV2.lpSafeEth],
-    [contracts.mainnet.uniswapV2.router2]
-  ),
-  allow.mainnet.uniswapV2.router2.removeLiquidityETH(
-    SAFE,
-    undefined,
-    undefined,
-    undefined,
-    c.avatar
-  ),
-] satisfies PermissionList
+]
