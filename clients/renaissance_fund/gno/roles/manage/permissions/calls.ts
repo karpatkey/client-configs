8421e5abffbe8c06ccfb54d040c3344e0890f3b1
Jan-Felix <jfschwarz@users.noreply.github.com>
2025-01-28 17:33:47 +0100

    apply latest fix from multi-instance-support branch

--- clients/renaissance_fund/gno/roles/manage/permissions/calls.ts
+++ clients/renaissance_fund/gno/roles/manage/permissions/calls.ts
@@ -4,49 +4,50 @@ import { USDC, USDCe } from "@/addresses/gno"
 import { contracts } from "@/contracts"
 import { allowErc20Approve } from "@/helpers"
 import { PermissionList } from "@/types"
-import { avatar } from "../../../../addresses"
+import { Parameters } from "../../../../parameters"
 
-export default [
-  /*********************************************
-   * Typed-presets permissions
-   *********************************************/
-  // Wrapping and unwrapping of XDAI, WXDAI
-  allow.gnosis.wxdai.deposit({ send: true }),
-  allow.gnosis.wxdai.withdraw(),
+export default (parameters: Parameters) =>
+  [
+    /*********************************************
+     * Typed-presets permissions
+     *********************************************/
+    // Wrapping and unwrapping of XDAI, WXDAI
+    allow.gnosis.wxdai.deposit({ send: true }),
+    allow.gnosis.wxdai.withdraw(),
 
-  /*********************************************
-   * Swaps
-   *********************************************/
-  // Swap USDC.e -> USDC
-  ...allowErc20Approve([USDCe], [contracts.gnosis.usdcTransmuter]),
-  allow.gnosis.usdcTransmuter.withdraw(),
-  // Swap USDC -> USDC.e
-  ...allowErc20Approve([USDC], [contracts.gnosis.usdcTransmuter]),
-  allow.gnosis.usdcTransmuter.deposit(),
+    /*********************************************
+     * Swaps
+     *********************************************/
+    // Swap USDC.e -> USDC
+    ...allowErc20Approve([USDCe], [contracts.gnosis.usdcTransmuter]),
+    allow.gnosis.usdcTransmuter.withdraw(),
+    // Swap USDC -> USDC.e
+    ...allowErc20Approve([USDC], [contracts.gnosis.usdcTransmuter]),
+    allow.gnosis.usdcTransmuter.deposit(),
 
-  /*********************************************
-   * Bridge
-   *********************************************/
-  // NAV Calculator - bridgeStart - In the future, the bridged assets should be scoped appropriately.
-  allow.gnosis.navCalculator.bridgeStart(),
+    /*********************************************
+     * Bridge
+     *********************************************/
+    // NAV Calculator - bridgeStart - In the future, the bridged assets should be scoped appropriately.
+    allow.gnosis.navCalculator.bridgeStart(),
 
-  // Bridge - Gnosis -> Mainnet
-  // XDAI (Gnosis) -> DAI (Mainnet)
-  allow.gnosis.xdaiBridge2.relayTokens(c.avatar, {
-    send: true,
-  }),
+    // Bridge - Gnosis -> Mainnet
+    // XDAI (Gnosis) -> DAI (Mainnet)
+    allow.gnosis.xdaiBridge2.relayTokens(c.avatar, {
+      send: true,
+    }),
 
-  // USDC (Gnosis) -> USDC (Mainnet)
-  allow.gnosis.usdc.transferAndCall(
-    contracts.gnosis.xdaiBridge,
-    undefined,
-    avatar
-  ),
+    // USDC (Gnosis) -> USDC (Mainnet)
+    allow.gnosis.usdc.transferAndCall(
+      contracts.gnosis.xdaiBridge,
+      undefined,
+      parameters.avatar
+    ),
 
-  // GNO (Gnosis) -> GNO (Mainnet)
-  allow.gnosis.gno.transferAndCall(
-    contracts.gnosis.xdaiBridge,
-    undefined,
-    avatar
-  ),
-] satisfies PermissionList
+    // GNO (Gnosis) -> GNO (Mainnet)
+    allow.gnosis.gno.transferAndCall(
+      contracts.gnosis.xdaiBridge,
+      undefined,
+      parameters.avatar
+    ),
+  ] satisfies PermissionList
