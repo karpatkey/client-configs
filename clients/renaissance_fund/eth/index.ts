f32761f9d1ce26178c20881615f59e95d78a9692
Jan-Felix <jfschwarz@users.noreply.github.com>
2025-01-28 17:33:47 +0100

    fix missing export

--- clients/renaissance_fund/eth/index.ts
+++ clients/renaissance_fund/eth/index.ts
@@ -1,7 +1,4 @@
-// Test-Safe Roles Mod v2: 0xBd1099dFD3c11b65FB4BB19A350da2f5B61Efb0d
-// Renaissance Fund Roles Mod v2: 0x94aC94443C38F5117dD003Dc7c9Fd3511702b280
-export const rolesMod = "0x94aC94443C38F5117dD003Dc7c9Fd3511702b280"
-
 export const chainId = 1
 
 export * as roles from "./roles"
+export * as instances from "../instances"
