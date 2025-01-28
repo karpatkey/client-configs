0314bca7b2ea59ff43e7ca659abbdf3b61395d1c
Jan-Felix <jfschwarz@users.noreply.github.com>
2025-01-28 17:33:47 +0100

    migrate renaissance

---
+++ clients/renaissance_fund/eth/roles/manage/permissions/_actions.ts
@@ -0,0 +1,113 @@
+import { allow as allowAction } from "defi-kit/eth"
+import {
+  _1INCH,
+  AAVE,
+  BAL,
+  COW,
+  CRV,
+  DAI,
+  DYDX,
+  ENA,
+  GHO,
+  GNO,
+  LDO,
+  LINK,
+  MKR,
+  ONDO,
+  PENDLE,
+  PYTH,
+  RPL,
+  SAFE,
+  SKY,
+  sUSDS,
+  stkAAVE,
+  stkGHO,
+  USDC,
+  USDS,
+  USDT,
+  UNI,
+} from "@/addresses/eth"
+import { kpkGovernance } from "../../../../addresses"
+
+export default [
+  // Aave Safety Module - Stake AAVE
+  allowAction.aave_v3.stake({ targets: ["AAVE"] }),
+  // Aave Safety Module - Stake GHO
+  allowAction.aave_v3.stake({ targets: ["GHO"] }),
+  // Aave v3 - Delegate AAVE to governance.karpatkey.eth
+  allowAction.aave_v3.delegate({ targets: ["AAVE"], delegatee: kpkGovernance }),
+  // Aave v3 - Delegate stkAAVE to governance.karpatkey.eth
+  allowAction.aave_v3.delegate({
+    targets: ["stkAAVE"],
+    delegatee: kpkGovernance,
+  }),
+  // Aave v3 - Delegate aAAVE to governance.karpatkey.eth
+  allowAction.aave_v3.delegate({
+    targets: ["aEthAAVE"],
+    delegatee: kpkGovernance,
+  }),
+
+  // Spark - SKY_USDS
+  allowAction.spark.deposit({ targets: ["SKY_USDS"] }),
+
+  // CowSwap - [1INCH, AAVE, BAL, COW, CRV, DAI, DYDX, ENA, GHO, GNO, LDO, LINK, MKR, ONDO, PENDLE, PYTH, RPL, SAFE, SKY, sUSDS, stkAAVE, stkGHO, USDC, USDS, USDT, UNI] <->
+  // [1INCH, AAVE, BAL, COW, CRV, DAI, DYDX, ENA, GHO, GNO, LDO, LINK, MKR, ONDO, PENDLE, PYTH, RPL, SAFE, SKY, sUSDS, stkAAVE, stkGHO, USDC, USDS, USDT, UNI]
+  allowAction.cowswap.swap({
+    sell: [
+      _1INCH,
+      AAVE,
+      BAL,
+      COW,
+      CRV,
+      DAI,
+      DYDX,
+      ENA,
+      GHO,
+      GNO,
+      LDO,
+      LINK,
+      MKR,
+      ONDO,
+      PENDLE,
+      PYTH,
+      RPL,
+      SAFE,
+      SKY,
+      sUSDS,
+      stkAAVE,
+      stkGHO,
+      USDC,
+      USDS,
+      USDT,
+      UNI,
+    ],
+    buy: [
+      _1INCH,
+      AAVE,
+      BAL,
+      COW,
+      CRV,
+      DAI,
+      DYDX,
+      ENA,
+      GHO,
+      GNO,
+      LDO,
+      LINK,
+      MKR,
+      ONDO,
+      PENDLE,
+      PYTH,
+      RPL,
+      SAFE,
+      SKY,
+      sUSDS,
+      stkAAVE,
+      stkGHO,
+      USDC,
+      USDS,
+      USDT,
+      UNI,
+    ],
+  }),
+]
