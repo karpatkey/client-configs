0d5b61224799f0d7bca4223bcf24a0e87886e3b5
Jan-Felix <jfschwarz@users.noreply.github.com>
2025-01-29 22:13:54 +0100

    bring gnosis_dao configs into new structure

--- clients/gnosis_dao/gno/index.ts
+++ clients/gnosis_dao/gno/index.ts
@@ -1,7 +1,4 @@
-export const avatar = "0x458cD345B4C05e8DF39d0A07220feb4Ec19F5e6f"
-
-export const rolesMod = "0x8faeEeFD8D01a92300011b46D366F0E7da2753ef"
-
 export const chainId = 100
 
 export * as roles from "./roles"
+export * as instances from "./instances"
