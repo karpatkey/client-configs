f83a86b7772329789320549d89d964d297e5c272
Jan-Felix <jfschwarz@users.noreply.github.com>
2025-01-31 17:10:20 +0100

    cleanup

--- clients/cow/main/eth/index.ts
+++ clients/cow/main/eth/index.ts
@@ -1,4 +1,5 @@
 export const avatar = "0x616dE58c011F8736fa20c7Ae5352F7f6FB9F0669"
+
 export const legalDefenseFund = "0x7f8987d6a8bee31bd7be80e877732579e2582a28"
 export const buyBack = "0xA2BFc70e63a48BDfc3F8013a44B04b6c33fb8200"
 
