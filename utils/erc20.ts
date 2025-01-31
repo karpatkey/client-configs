1fb7d9a5c80a25f258c8d4ecc3a659ed6fbb0e30
Jan-Felix <jfschwarz@users.noreply.github.com>
2025-01-31 14:41:22 +0100

    migrate cow fee safe

--- utils/erc20.ts
+++ utils/erc20.ts
@@ -15,3 +15,19 @@ export const allowErc20Approve = (
       ["address", "uint256"]
     ),
   })
+
+export const allowErc20Transfer = (
+  tokens: readonly `0x${string}`[],
+  spenders: readonly `0x${string}`[]
+) =>
+  forAll(tokens, {
+    signature: "approve(address,uint256)",
+    condition: c.calldataMatches(
+      [
+        spenders.length === 1
+          ? spenders[0]
+          : c.or(...(spenders as [string, string, ...string[]])),
+      ],
+      ["address", "uint256"]
+    ),
+  })
