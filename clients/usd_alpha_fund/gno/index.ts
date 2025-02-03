06aa172fd4394973cdf68438f94404468efe355b
Jan-Felix <jfschwarz@users.noreply.github.com>
2025-02-03 15:28:30 +0100

    migrate usd alpha fund

--- clients/usd_alpha_fund/gno/index.ts
+++ clients/usd_alpha_fund/gno/index.ts
@@ -1,9 +1,4 @@
-export const avatar = "0xdF8EE91120154BdC3Cb628f0535B6511e52327Ff"
-
-// Test-Safe Roles Mod v2: 0xBd1099dFD3c11b65FB4BB19A350da2f5B61Efb0d
-// Institutional Alpha Fund Roles Mod v2: 0xa14ed1760Ab65474D1f28836A5B8489ff604c4a9
-export const rolesMod = "0xa14ed1760Ab65474D1f28836A5B8489ff604c4a9"
-
 export const chainId = 100
 
 export * as roles from "./roles"
+export * as instances from "../instances"
---
+++ clients/usd_alpha_fund/instances/main.ts
@@ -0,0 +1,1 @@
+export const rolesMod = "0xa14ed1760Ab65474D1f28836A5B8489ff604c4a9"
