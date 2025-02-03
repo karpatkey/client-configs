2ab374451b2885ab5c88fc7bd81b95a8c5c28b1b
Jan-Felix <jfschwarz@users.noreply.github.com>
2025-02-03 14:59:34 +0100

    migrate eth_lst_fund

--- clients/eth_lst_fund/eth/index.ts
+++ clients/eth_lst_fund/eth/index.ts
@@ -1,7 +1,4 @@
-export const avatar = "0x568240DefD76d42fcf6a3347ba4f5B0ABDC4a11A"
-
-export const rolesMod = "0xd924cb41E893Eb0AF5A69f92810a0C5D88140187"
-
 export const chainId = 1
 
 export * as roles from "./roles"
+export * as instances from "./instances"
