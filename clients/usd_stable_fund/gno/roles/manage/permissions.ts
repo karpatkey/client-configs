26c03618fb734d5e5803038e9d4c13c05d40b510
Jan-Felix <jfschwarz@users.noreply.github.com>
2025-02-03 18:23:05 +0100

    migrate usd stable fund

--- clients/usd_stable_fund/gno/roles/manage/permissions.ts
+++ clients/usd_stable_fund/gno/roles/manage/permissions.ts
@@ -5,107 +5,108 @@ import { sDAI, USDC, USDCe, WXDAI, balancer } from "@/addresses/gno"
 import { contracts } from "@/contracts"
 import { allowErc20Approve } from "@/helpers"
 import { PermissionList } from "@/types"
-import { avatar } from "../../index"
 import { balancerSwap } from "@/exit_strategies/balancer"
 import { eAddress } from "@/addresses"
+import { Parameters } from "../../../parameters"
 
-export default [
-  /*********************************************
-   * DeFi-Kit permissions
-   *********************************************/
-  // Aave v3 - Deposit sDAI
-  allowAction.aave_v3.deposit({ targets: ["sDAI"] }),
-  // Aave v3 - Deposit USDC
-  allowAction.aave_v3.deposit({ targets: ["USDC"] }),
-  // Aave v3 - Deposit WXDAI
-  allowAction.aave_v3.deposit({ targets: ["WXDAI"] }),
-  // Aave v3 - Deposit XDAI
-  allowAction.aave_v3.deposit({ targets: ["XDAI"] }),
-  // Aave v3 - Borrow USDC
-  allowAction.aave_v3.borrow({ targets: ["USDC"] }),
-  // Aave v3 - Borrow WXDAI
-  allowAction.aave_v3.borrow({ targets: ["WXDAI"] }),
-  // Aave v3 - Borrow XDAI
-  allowAction.aave_v3.borrow({ targets: ["XDAI"] }),
+export default (parameters: Parameters) =>
+  [
+    /*********************************************
+     * DeFi-Kit permissions
+     *********************************************/
+    // Aave v3 - Deposit sDAI
+    allowAction.aave_v3.deposit({ targets: ["sDAI"] }),
+    // Aave v3 - Deposit USDC
+    allowAction.aave_v3.deposit({ targets: ["USDC"] }),
+    // Aave v3 - Deposit WXDAI
+    allowAction.aave_v3.deposit({ targets: ["WXDAI"] }),
+    // Aave v3 - Deposit XDAI
+    allowAction.aave_v3.deposit({ targets: ["XDAI"] }),
+    // Aave v3 - Borrow USDC
+    allowAction.aave_v3.borrow({ targets: ["USDC"] }),
+    // Aave v3 - Borrow WXDAI
+    allowAction.aave_v3.borrow({ targets: ["WXDAI"] }),
+    // Aave v3 - Borrow XDAI
+    allowAction.aave_v3.borrow({ targets: ["XDAI"] }),
 
-  // CowSwap - [sDAI, USDC, USDCe, WXDAI] -> [eAddress, sDAI, USDC, USDCe, WXDAI]
-  allowAction.cowswap.swap({
-    sell: [sDAI, USDC, USDCe, WXDAI],
-    buy: [eAddress, sDAI, USDC, USDCe, WXDAI],
-  }),
+    // CowSwap - [sDAI, USDC, USDCe, WXDAI] -> [eAddress, sDAI, USDC, USDCe, WXDAI]
+    allowAction.cowswap.swap({
+      sell: [sDAI, USDC, USDCe, WXDAI],
+      buy: [eAddress, sDAI, USDC, USDCe, WXDAI],
+    }),
 
-  // Spark - DSR_sDAI
-  allowAction.spark.deposit({ targets: ["DSR_sDAI"] }),
-  // Spark - Deposit sDAI
-  allowAction.spark.deposit({ targets: ["sDAI"] }),
-  // Spark - Deposit USDC
-  allowAction.spark.deposit({ targets: ["USDC"] }),
-  // Spark - Deposit WXDAI
-  allowAction.spark.deposit({ targets: ["WXDAI"] }),
-  // Spark - Deposit XDAI
-  allowAction.spark.deposit({ targets: ["XDAI"] }),
-  // Spark - Borrow USDC
-  allowAction.spark.borrow({ targets: ["USDC"] }),
-  // Spark - Borrow WXDAI
-  allowAction.spark.borrow({ targets: ["WXDAI"] }),
-  // Spark - Borrow XDAI
-  allowAction.spark.borrow({ targets: ["XDAI"] }),
+    // Spark - DSR_sDAI
+    allowAction.spark.deposit({ targets: ["DSR_sDAI"] }),
+    // Spark - Deposit sDAI
+    allowAction.spark.deposit({ targets: ["sDAI"] }),
+    // Spark - Deposit USDC
+    allowAction.spark.deposit({ targets: ["USDC"] }),
+    // Spark - Deposit WXDAI
+    allowAction.spark.deposit({ targets: ["WXDAI"] }),
+    // Spark - Deposit XDAI
+    allowAction.spark.deposit({ targets: ["XDAI"] }),
+    // Spark - Borrow USDC
+    allowAction.spark.borrow({ targets: ["USDC"] }),
+    // Spark - Borrow WXDAI
+    allowAction.spark.borrow({ targets: ["WXDAI"] }),
+    // Spark - Borrow XDAI
+    allowAction.spark.borrow({ targets: ["XDAI"] }),
 
-  /*********************************************
-   * Typed-presets permissions
-   *********************************************/
-  // Wrapping and unwrapping of XDAI, WXDAI
-  allow.gnosis.wxdai.deposit({ send: true }),
-  allow.gnosis.wxdai.withdraw(),
+    /*********************************************
+     * Typed-presets permissions
+     *********************************************/
+    // Wrapping and unwrapping of XDAI, WXDAI
+    allow.gnosis.wxdai.deposit({ send: true }),
+    allow.gnosis.wxdai.withdraw(),
 
-  /*********************************************
-   * Swaps
-   *********************************************/
-  // Balancer - sDAI <-> USDC
-  balancerSwap(balancer.sBal3Pid, [sDAI, USDC], [sDAI, USDC]),
+    /*********************************************
+     * Swaps
+     *********************************************/
+    // Balancer - sDAI <-> USDC
+    balancerSwap(balancer.sBal3Pid, [sDAI, USDC], [sDAI, USDC]),
 
-  // Balancer - sDAI <-> USDC.e
-  balancerSwap(balancer.sBAL3NewPid, [sDAI, USDCe], [sDAI, USDCe]),
+    // Balancer - sDAI <-> USDC.e
+    balancerSwap(balancer.sBAL3NewPid, [sDAI, USDCe], [sDAI, USDCe]),
 
-  // Balancer - USDC <-> WXDAI
-  balancerSwap(balancer.staBal3Pid, [USDC, WXDAI], [USDC, WXDAI]),
+    // Balancer - USDC <-> WXDAI
+    balancerSwap(balancer.staBal3Pid, [USDC, WXDAI], [USDC, WXDAI]),
 
-  // Swap USDC.e -> USDC
-  ...allowErc20Approve([USDCe], [contracts.gnosis.usdcTransmuter]),
-  allow.gnosis.usdcTransmuter.withdraw(),
-  // Swap USDC -> USDC.e
-  ...allowErc20Approve([USDC], [contracts.gnosis.usdcTransmuter]),
-  allow.gnosis.usdcTransmuter.deposit(),
+    // Swap USDC.e -> USDC
+    ...allowErc20Approve([USDCe], [contracts.gnosis.usdcTransmuter]),
+    allow.gnosis.usdcTransmuter.withdraw(),
+    // Swap USDC -> USDC.e
+    ...allowErc20Approve([USDC], [contracts.gnosis.usdcTransmuter]),
+    allow.gnosis.usdcTransmuter.deposit(),
 
-  /*********************************************
-   * Bridge
-   *********************************************/
-  // NAV Calculator - bridgeStart - In the future, the bridged assets should be scoped appropriately.
-  allow.gnosis.navCalculator.bridgeStart(),
+    /*********************************************
+     * Bridge
+     *********************************************/
+    // NAV Calculator - bridgeStart - In the future, the bridged assets should be scoped appropriately.
+    allow.gnosis.navCalculator.bridgeStart(),
 
-  // Bridge - Gnosis -> Mainnet
-  // XDAI (Gnosis) -> DAI (Mainnet)
-  allow.gnosis.xdaiBridge2.relayTokens(c.avatar, {
-    send: true,
-  }),
-  // XDAI (Gnosis) -> DAI (Mainnet) - HOP
-  allow.gnosis.hopDaiWrapper.swapAndSend(
-    1, // Mainnet
-    c.avatar
-  ),
+    // Bridge - Gnosis -> Mainnet
+    // XDAI (Gnosis) -> DAI (Mainnet)
+    allow.gnosis.xdaiBridge2.relayTokens(c.avatar, {
+      send: true,
+    }),
+    // XDAI (Gnosis) -> DAI (Mainnet) - HOP
+    allow.gnosis.hopDaiWrapper.swapAndSend(
+      1, // Mainnet
+      c.avatar
+    ),
 
-  // // COMP (Gnosis) -> COMP (Mainnet)
-  // allow.gnosis.comp.transferAndCall(
-  //   contracts.gnosis.xdaiBridge,
-  //   undefined,
-  //   avatar
-  // ),
+    // // COMP (Gnosis) -> COMP (Mainnet)
+    // allow.gnosis.comp.transferAndCall(
+    //   contracts.gnosis.xdaiBridge,
+    //   undefined,
+    //   avatar
+    // ),
 
-  // USDC (Gnosis) -> USDC (Mainnet)
-  allow.gnosis.usdc.transferAndCall(
-    contracts.gnosis.xdaiBridge,
-    undefined,
-    avatar
-  ),
-  // HOP does not work with USDC and USDC.e
-] satisfies PermissionList
+    // USDC (Gnosis) -> USDC (Mainnet)
+    allow.gnosis.usdc.transferAndCall(
+      contracts.gnosis.xdaiBridge,
+      undefined,
+      parameters.avatar
+    ),
+    // HOP does not work with USDC and USDC.e
+  ] satisfies PermissionList
--- clients/usd_stable_fund/oeth/index.ts
+++ clients/usd_stable_fund/oeth/index.ts
@@ -1,11 +1,4 @@
-// Test safe: 0xd0ca2a7Ed8aeE7972750B085B27350f1Cd387F9b
-// Avatar safe: 0x846E7F810E08F1E2AF2c5AfD06847cc95F5CaE1B
-export const avatar = "0x846E7F810E08F1E2AF2c5AfD06847cc95F5CaE1B"
-
-// Test roles: 0xbF956d2E355A07ac1648DeD28E339CD1227Cd45D
-// KPK roles: 0xD1F7cd1c68a7f82AD86D320D2446eb706822f742
-export const rolesMod = "0xD1F7cd1c68a7f82AD86D320D2446eb706822f742"
-
 export const chainId = 10
 
 export * as roles from "./roles"
+export * as instances from "./instances"
