2053dc6c1d44efb9104a4b745c93aa90f78b22c5
Jan-Felix <jfschwarz@users.noreply.github.com>
2025-02-03 18:23:05 +0100

    migrate usd stable fund

---
+++ clients/usd_stable_fund/eth/instances/test.ts
@@ -0,0 +1,7 @@
+import { Parameters } from "../../parameters"
+
+export const rolesMod = "0x53253c670e079274e13A5E1b462E7f6E8b6a4B40"
+
+export const parameters: Parameters = {
+  avatar: "0xd0ca2a7Ed8aeE7972750B085B27350f1Cd387F9b",
+}
