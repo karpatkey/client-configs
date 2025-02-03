2c5c319c0689fc37c46c35b53b4ff28d4b74783e
Jan-Felix <jfschwarz@users.noreply.github.com>
2025-02-03 15:11:58 +0100

    migrate nexus

--- clients/nexus/eth/index.ts
+++ clients/nexus/eth/index.ts
@@ -1,11 +1,4 @@
-// Nexus Avatar: 0x8e53D04644E9ab0412a8c6bd228C84da7664cFE3
-// Test-Safe: 0xC01318baB7ee1f5ba734172bF7718b5DC6Ec90E1
-// export const avatar = "0xC01318baB7ee1f5ba734172bF7718b5DC6Ec90E1"
-
-// Test-Safe Roles Mod v2: 0xBd1099dFD3c11b65FB4BB19A350da2f5B61Efb0d
-// Nexus Roles Mod v2: 0x24Fc0B9414Bb8e99a822a45BdE3A398BcfC85A58
-export const rolesMod = "0x24Fc0B9414Bb8e99a822a45BdE3A398BcfC85A58"
-
 export const chainId = 1
 
 export * as roles from "./roles"
+export * as instances from "./instances"
