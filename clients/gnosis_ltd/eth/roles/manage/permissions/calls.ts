26fc7c3ace876bef33ea2d29664bbb3b5ba402cd
Jan-Felix <jfschwarz@users.noreply.github.com>
2025-02-04 12:53:34 +0100

    migrate gnosis ltd

--- clients/gnosis_ltd/eth/roles/manage/permissions/calls.ts
+++ clients/gnosis_ltd/eth/roles/manage/permissions/calls.ts
@@ -1,52 +1,13 @@
 import { c } from "zodiac-roles-sdk"
 import { allow } from "zodiac-roles-sdk/kit"
-import { allow as allowAction } from "defi-kit/eth"
 import { contracts } from "@/contracts"
 import { AZUR, DAI, GRT, sDAI, USDC, USDT } from "@/addresses/eth"
 import { zeroAddress } from "@/addresses"
 import { allowErc20Approve } from "@/helpers"
 import { PermissionList } from "@/types"
-import { graphDelegatee, gnosisLtdArb } from "../../addresses"
+import { graphDelegatee, gnosisLtdArb } from "../../../addresses"
 
 export default [
-  /*********************************************
-   * DeFi-Kit permissions
-   *********************************************/
-  // Aura - Lock
-  allowAction.aura.lock(),
-
-  // CowSwap - DAI <-> USDC
-  allowAction.cowswap.swap({
-    sell: [DAI, USDC],
-    buy: [DAI, USDC],
-  }),
-  // CowSwap - DAI <-> USDT
-  allowAction.cowswap.swap({
-    sell: [DAI, USDT],
-    buy: [DAI, USDT],
-  }),
-  // CowSwap - USDC <-> USDT
-  allowAction.cowswap.swap({
-    sell: [USDC, USDT],
-    buy: [USDC, USDT],
-  }),
-  // CowSwap - sDAI <-> USDC
-  allowAction.cowswap.swap({
-    sell: [sDAI, USDC],
-    buy: [sDAI, USDC],
-  }),
-  // CowSwap - sDAI <-> USDT
-  allowAction.cowswap.swap({
-    sell: [sDAI, USDT],
-    buy: [sDAI, USDT],
-  }),
-
-  // Lido
-  allowAction.lido.deposit(),
-
-  // Spark - DSR_sDAI
-  allowAction.spark.deposit({ targets: ["DSR_sDAI"] }),
-
   /*********************************************
    * Typed-presets permissions
    *********************************************/
