09b6df28a62537404c272fa9b2fc88f82802b2c8
Jan-Felix <jfschwarz@users.noreply.github.com>
2025-02-04 12:57:08 +0100

    migrate cow

--- clients/cow/main/eth/roles/disassemble/permissions.ts
+++
@@ -1,6 +0,0 @@
-import { c } from "zodiac-roles-sdk"
-import { allow } from "zodiac-roles-sdk/kit"
-import { contracts } from "@/contracts"
-import { PermissionList } from "@/types"
-
-export default [] satisfies PermissionList
