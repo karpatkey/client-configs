d1f3d4890e6a599edbdc0b28d854f08455bd5f7d
nledo <nicolas@karpatkey.com>
2025-02-06 10:07:14 -0300

    Fixes

--- clients/renaissance_fund/gno/roles/manage/permissions.ts
+++ clients/renaissance_fund/gno/roles/manage/permissions.ts
@@ -5,68 +5,69 @@ import { GNO, osGNO, sDAI, USDC, USDCe, WXDAI } from "@/addresses/gno"
 import { contracts } from "@/contracts"
 import { allowErc20Approve } from "@/helpers"
 import { PermissionList } from "@/types"
-import { avatar } from "../../index"
+import { Parameters } from "../../../parameters"
 
-export default [
-  /*********************************************
-   * DeFi-Kit permissions
-   *********************************************/
-  // CowSwap - [GNO, osGNO, sDAI, USDC, USDC.e, WXDAI, xDAI] <-> [GNO, osGNO, sDAI, USDC, USDC.e, WXDAI, xDAI]
-  allowAction.cowswap.swap({
-    sell: ["XDAI", GNO, osGNO, sDAI, USDC, USDCe, WXDAI],
-    buy: ["XDAI", GNO, osGNO, sDAI, USDC, USDCe, WXDAI],
-  }),
+export default (parameters: Parameters) =>
+  [
+    /*********************************************
+     * DeFi-Kit permissions
+     *********************************************/
+    // CowSwap - [GNO, osGNO, sDAI, USDC, USDC.e, WXDAI, xDAI] <-> [GNO, osGNO, sDAI, USDC, USDC.e, WXDAI, xDAI]
+    allowAction.cowswap.swap({
+      sell: ["XDAI", GNO, osGNO, sDAI, USDC, USDCe, WXDAI],
+      buy: ["XDAI", GNO, osGNO, sDAI, USDC, USDCe, WXDAI],
+    }),
 
-  // Spark - DSR_sDAI
-  allowAction.spark.deposit({ targets: ["DSR_sDAI"] }),
+    // Spark - DSR_sDAI
+    allowAction.spark.deposit({ targets: ["DSR_sDAI"] }),
 
-  // StakeWise v3 - Genesis
-  allowAction.stakewise_v3.stake({ targets: ["Genesis Vault"] }),
-  // StakeWise v3 - Serenita
-  allowAction.stakewise_v3.stake({ targets: ["Serenita"] }),
-  // StakeWise v3 - NEDO
-  allowAction.stakewise_v3.stake({ targets: ["NEDO"] }),
+    // StakeWise v3 - Genesis
+    allowAction.stakewise_v3.stake({ targets: ["Genesis Vault"] }),
+    // StakeWise v3 - Serenita
+    allowAction.stakewise_v3.stake({ targets: ["Serenita"] }),
+    // StakeWise v3 - NEDO
+    allowAction.stakewise_v3.stake({ targets: ["NEDO"] }),
 
-  /*********************************************
-   * Typed-presets permissions
-   *********************************************/
-  // Wrapping and unwrapping of XDAI, WXDAI
-  allow.gnosis.wxdai.deposit({ send: true }),
-  allow.gnosis.wxdai.withdraw(),
+    /*********************************************
+     * Typed-presets permissions
+     *********************************************/
+    // Wrapping and unwrapping of XDAI, WXDAI
+    allow.gnosis.wxdai.deposit({ send: true }),
+    allow.gnosis.wxdai.withdraw(),
 
-  /*********************************************
-   * Swaps
-   *********************************************/
-  // Swap USDC.e -> USDC
-  ...allowErc20Approve([USDCe], [contracts.gnosis.usdcTransmuter]),
-  allow.gnosis.usdcTransmuter.withdraw(),
-  // Swap USDC -> USDC.e
-  ...allowErc20Approve([USDC], [contracts.gnosis.usdcTransmuter]),
-  allow.gnosis.usdcTransmuter.deposit(),
+    /*********************************************
+     * Swaps
+     *********************************************/
+    // Swap USDC.e -> USDC
+    ...allowErc20Approve([USDCe], [contracts.gnosis.usdcTransmuter]),
+    allow.gnosis.usdcTransmuter.withdraw(),
+    // Swap USDC -> USDC.e
+    ...allowErc20Approve([USDC], [contracts.gnosis.usdcTransmuter]),
+    allow.gnosis.usdcTransmuter.deposit(),
 
-  /*********************************************
-   * Bridge
-   *********************************************/
-  // NAV Calculator - bridgeStart - In the future, the bridged assets should be scoped appropriately.
-  allow.gnosis.navCalculator.bridgeStart(),
+    /*********************************************
+     * Bridge
+     *********************************************/
+    // NAV Calculator - bridgeStart - In the future, the bridged assets should be scoped appropriately.
+    allow.gnosis.navCalculator.bridgeStart(),
 
-  // Bridge - Gnosis -> Mainnet
-  // XDAI (Gnosis) -> DAI (Mainnet)
-  allow.gnosis.xdaiBridge2.relayTokens(c.avatar, {
-    send: true,
-  }),
+    // Bridge - Gnosis -> Mainnet
+    // XDAI (Gnosis) -> DAI (Mainnet)
+    allow.gnosis.xdaiBridge2.relayTokens(c.avatar, {
+      send: true,
+    }),
 
-  // USDC (Gnosis) -> USDC (Mainnet)
-  allow.gnosis.usdc.transferAndCall(
-    contracts.gnosis.xdaiBridge,
-    undefined,
-    avatar
-  ),
+    // USDC (Gnosis) -> USDC (Mainnet)
+    allow.gnosis.usdc.transferAndCall(
+      contracts.gnosis.xdaiBridge,
+      undefined,
+      parameters.avatar
+    ),
 
-  // GNO (Gnosis) -> GNO (Mainnet)
-  allow.gnosis.gno.transferAndCall(
-    contracts.gnosis.xdaiBridge,
-    undefined,
-    avatar
-  ),
-] satisfies PermissionList
+    // GNO (Gnosis) -> GNO (Mainnet)
+    allow.gnosis.gno.transferAndCall(
+      contracts.gnosis.xdaiBridge,
+      undefined,
+      parameters.avatar
+    ),
+  ] satisfies PermissionList
