382463513752f8c05e85fff1f7804ed63ee72fa7
Jan-Felix <jfschwarz@users.noreply.github.com>
2025-02-18 14:28:20 +0100

    add more scoped allow eth transfer helper

--- clients/cow/fee/eth/roles/manage/permissions/calls.ts
+++ clients/cow/fee/eth/roles/manage/permissions/calls.ts
@@ -1,7 +1,7 @@
 import { allow } from "zodiac-roles-sdk/kit"
 import { WETH } from "@/addresses/eth"
 import { mainTreasury } from "../../../../../addresses"
-import { allowEthTransfer } from "@/helpers"
+import { allowEthTransferWithAnyData } from "@/helpers"
 import { PermissionList } from "@/types"
 import { allowErc20Transfer } from "@/helpers"
 
@@ -19,7 +19,7 @@ export default [
    * Transfers
    *********************************************/
   // Transfer ETH to Main Treasury
-  allowEthTransfer(mainTreasury),
+  allowEthTransferWithAnyData(mainTreasury),
   // Transfer WETH to Main Treasury
   allowErc20Transfer([WETH], [mainTreasury]),
 ] satisfies PermissionList
