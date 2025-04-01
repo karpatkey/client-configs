134a41a4863d9ed5a3973d195c07b423dcd5d9c8
Skwst <31214849+Skwst@users.noreply.github.com>
2025-04-01 13:59:31 +0300

    aave v3 withdraw

--- clients/safe/mainnet/roles/MANAGER/permissions/calls.ts
+++ clients/safe/mainnet/roles/MANAGER/permissions/calls.ts
@@ -1,6 +1,6 @@
 import { c } from "zodiac-roles-sdk"
 import { allow } from "zodiac-roles-sdk/kit"
-import { SAFE, uniswapV2 } from "@/addresses/eth"
+import { DAI, SAFE, uniswapV2, USDC, USDS, USDT, WETH } from "@/addresses/eth"
 import { contracts } from "@/contracts"
 import { allowErc20Approve } from "@/helpers"
 import { PermissionList } from "@/types"
@@ -9,6 +9,17 @@ export default [
   /*********************************************
    * Typed-presets permissions
    *********************************************/
+  // Aave v3 - Withdraw USDC
+  allow.mainnet.aaveV3.poolCoreV3.withdraw(USDC, undefined, c.avatar),
+  // Aave v3 - Withdraw USDS
+  allow.mainnet.aaveV3.poolCoreV3.withdraw(USDS, undefined, c.avatar),
+  // Aave v3 - Withdraw DAI
+  allow.mainnet.aaveV3.poolCoreV3.withdraw(DAI, undefined, c.avatar),
+  // Aave v3 - Withdraw USDT
+  allow.mainnet.aaveV3.poolCoreV3.withdraw(USDT, undefined, c.avatar),
+  // Aave v3 - Withdraw WETH
+  allow.mainnet.aaveV3.poolCoreV3.withdraw(WETH, undefined, c.avatar),
+  
   // Uniswap v2 - SAFE/WETH
   allowErc20Approve([SAFE], [contracts.mainnet.uniswapV2.router2]),
   allow.mainnet.uniswapV2.router2.addLiquidityETH(
