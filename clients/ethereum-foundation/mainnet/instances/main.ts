f4bb9b1b602d054e7f845c5e75cd7ad33d06b923
nledo <nicolas@karpatkey.com>
2025-04-01 14:57:08 -0300

    ethereum foundation setup

--- clients/ethereum-foundation/mainnet/instances/main.ts
+++ clients/ethereum-foundation/mainnet/instances/main.ts
@@ -1,4 +1,4 @@
-export const rolesMod = "0x32E13B86491a740D1F543CC0bCD5c8ae15ac3d5F"
+export const rolesMod = "0x9ccf6E04697baad68E3EE7FCB624eeC760ACA897"
 export const chainId = 1
 
 export const avatar = "0x9fC3dc011b461664c835F2527fffb1169b3C213e"
--- clients/ethereum-foundation/mainnet/roles/MANAGER/permissions/calls.ts
+++ clients/ethereum-foundation/mainnet/roles/MANAGER/permissions/calls.ts
@@ -4,27 +4,4 @@ import { contracts } from "@/contracts"
 import { PermissionList } from "@/types"
 
 export default [
-  // Compound v3 - Deposit ETH
-  allow.mainnet.compoundV3.cWethV3.allow(
-    contracts.mainnet.compoundV3.mainnetBulker
-  ),
-  allow.mainnet.compoundV3.mainnetBulker.invoke(
-    c.every(
-      c.or(
-        c.eq(
-          "0x414354494f4e5f535550504c595f4e41544956455f544f4b454e000000000000" // ACTION_SUPPLY_NATIVE_TOKEN
-        ),
-        c.eq(
-          "0x414354494f4e5f57495448445241575f4e41544956455f544f4b454e00000000" // ACTION_WITHDRAW_NATIVE_TOKEN
-        )
-      )
-    ),
-    c.every(
-      c.abiEncodedMatches(
-        [contracts.mainnet.compoundV3.cWethV3, c.avatar],
-        ["address", "address", "uint256"]
-      )
-    ),
-    { send: true }
-  ),
 ] satisfies PermissionList
