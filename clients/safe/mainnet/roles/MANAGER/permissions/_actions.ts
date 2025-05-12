f5632a4c8cecda80fa51728dc27b5edd6f1566a6
nledo <103588782+nledo@users.noreply.github.com>
2025-05-12 17:08:26 -0300

    SAFE DAO policies (#95)
    * add idea to gitignore
    
    * allow deposit in aave v3
    
    * Format code with Prettier
    
    * allow deposit in spark
    
    * aave v3 withdraw
    
    * Format code with Prettier
    
    * remove unused
    
    * compound v3 deposit
    
    * Format code with Prettier
    
    * Small tweaks
    
    * Bump DeFi Kit version
    
    * aura mainnet actions
    
    * balancer v2 actionns
    
    * spark and rocket pool
    
    * Format code with Prettier
    
    * stader and stake wise v3
    
    * convex
    
    * uniswap v3
    
    * Format code with Prettier
    
    * swaps
    
    * Format code with Prettier
    
    * polishing
    
    * add gnosis structure
    
    * gnosis actions
    
    * Format code with Prettier
    
    * use bpt cause there are duplicate names
    
    * Format code with Prettier
    
    * Mainnet typed-presets progress
    
    * Format code with Prettier
    
    * add some permissions
    
    * remove nonexistent
    
    * Format code with Prettier
    
    * Mainnet typed-presets progress
    
    * balancer pools on gnosis
    
    * Mainnet typed-presets progress
    
    * Format code with Prettier
    
    * Minor tweaks
    
    * replace convex pool
    
    * Fixes
    
    * Fixes
    
    * Progress with asset bridging
    
    * Progress with asset bridging
    
    * Format code with Prettier
    
    * Progress with asset bridging
    
    * Format code with Prettier
    
    * Added StakeWise v3 Chorus one vault to Mainnet
    
    * Increase tests timeout
    
    * Tests timeout
    
    * Last updates to Safe policies on GC
    
    * Format code with Prettier
    
    * Addresses and deployments update
    
    * Format code with Prettier
    
    * Remove NAV
    
    * Remove unnecessary file tracking
    
    ---------
    
    Co-authored-by: Skwst <31214849+Skwst@users.noreply.github.com>
    Co-authored-by: GitHub Actions <actions@github.com>

--- clients/safe/mainnet/roles/MANAGER/permissions/_actions.ts
+++ clients/safe/mainnet/roles/MANAGER/permissions/_actions.ts
@@ -2,29 +2,59 @@ import { allow as allowAction } from "defi-kit/eth"
 import {
   AURA,
   BAL,
+  COMP,
+  COW,
+  CRV,
+  CVX,
   DAI,
+  ETHx,
+  GHO,
+  GNO,
+  LDO,
+  OETH,
+  OGN,
   osETH,
   rETH,
+  RPL,
   SAFE,
+  sDAI,
+  SKY,
   stETH,
+  sUSDS,
+  SWISE,
+  UNI,
   USDC,
+  USDM,
+  USDS,
+  USDT,
   WETH,
   wstETH,
 } from "@/addresses/eth"
 
 export default [
-  /*********************************************
-   * DeFi-Kit permissions
-   *********************************************/
-  // Aura - wstETH/WETH
-  allowAction.aura.deposit({ targets: ["153"] }),
   // Aura - rETH/WETH
   allowAction.aura.deposit({ targets: ["109"] }),
+  // Aura - wstETH/WETH
+  allowAction.aura.deposit({ targets: ["153"] }),
   // Aura - osETH/WETH
   allowAction.aura.deposit({ targets: ["179"] }),
+  // Aura - auraB-80GNO-20WETH
+  allowAction.aura.deposit({ targets: ["116"] }),
+  // Aura - auraETHx/wstETH
+  allowAction.aura.deposit({ targets: ["207"] }),
 
-  // Aave Safety Module - Stake GHO
-  allowAction.aave_v3.stake({ targets: ["GHO"] }),
+  // Aave v3 Core Market - Deposit DAI
+  allowAction.aave_v3.deposit({ market: "Core", targets: ["DAI"] }),
+  // Aave v3 Core Market - Deposit ETH
+  allowAction.aave_v3.deposit({ market: "Core", targets: ["ETH"] }),
+  // Aave v3 Core Market - Deposit USDC
+  allowAction.aave_v3.deposit({ market: "Core", targets: ["USDC"] }),
+  // Aave v3 Core Market - Deposit USDS
+  allowAction.aave_v3.deposit({ market: "Core", targets: ["USDS"] }),
+  // Aave v3 Core Market - Deposit USDT
+  allowAction.aave_v3.deposit({ market: "Core", targets: ["USDT"] }),
+  // Aave v3 Core Market - Deposit WETH
+  allowAction.aave_v3.deposit({ market: "Core", targets: ["WETH"] }),
 
   // Balancer v2 - wstETH/WETH
   allowAction.balancer_v2.deposit({ targets: ["wstETH-WETH-BPT"] }),
@@ -35,26 +65,151 @@ export default [
   // Balancer v2 - osETH/WETH
   allowAction.balancer_v2.deposit({ targets: ["osETH/wETH-BPT"] }),
   allowAction.balancer_v2.stake({ targets: ["osETH/wETH-BPT"] }),
+  // Balancer v2 - B-80GNO-20WETH
+  allowAction.balancer_v2.deposit({ targets: ["B-80GNO-20WETH"] }),
+  allowAction.balancer_v2.stake({ targets: ["B-80GNO-20WETH"] }),
+  // Balancer v2 - ETHx/wstETH
+  allowAction.balancer_v2.deposit({ targets: ["ETHx/wstETH"] }),
+  allowAction.balancer_v2.stake({ targets: ["ETHx/wstETH"] }),
+  // Balancer v2 - ezETH-WETH-BPT
+  allowAction.balancer_v2.deposit({ targets: ["ezETH-WETH-BPT"] }),
+  allowAction.balancer_v2.stake({ targets: ["ezETH-WETH-BPT"] }),
+  // Balancer v2 - B-stETH-STABLE
+  allowAction.balancer_v2.deposit({ targets: ["B-stETH-STABLE"] }),
+  allowAction.balancer_v2.stake({ targets: ["B-stETH-STABLE"] }),
 
-  // CowSwap - SAFE -> [DAI, ETH, stETH, USDC, WETH, wstETH]
+  // Compound v3 - Deposit USDC
+  allowAction.compound_v3.deposit({ targets: ["cUSDCv3"], tokens: ["USDC"] }),
+  // Compound v3 - Deposit USDS
+  allowAction.compound_v3.deposit({ targets: ["cUSDSv3"], tokens: ["USDS"] }),
+  // Compound v3 - Deposit USDT
+  allowAction.compound_v3.deposit({ targets: ["cUSDTv3"], tokens: ["USDT"] }),
+  // Compound v3 - Deposit WETH
+  allowAction.compound_v3.deposit({ targets: ["cWETHv3"], tokens: ["WETH"] }),
+
+  // Convex - Deposit DAI-USDC-USDT
+  allowAction.convex.deposit({ targets: ["9"] }),
+  // Convex - Deposit ETH-stETH
+  allowAction.convex.deposit({ targets: ["25"] }),
+  // Convex - Deposit oETH-ETH
+  allowAction.convex.deposit({ targets: ["174"] }),
+  // Convex - Deposit ETH+rETH
+  allowAction.convex.deposit({ targets: ["287"] }),
+
+  // Spark - Deposit DAI
+  allowAction.spark.deposit({ targets: ["DAI"] }),
+  // Spark - Deposit ETH
+  allowAction.spark.deposit({ targets: ["ETH"] }),
+  // Spark - Deposit USDC
+  allowAction.spark.deposit({ targets: ["USDC"] }),
+  // Spark - Deposit USDS
+  allowAction.spark.deposit({ targets: ["USDS"] }),
+  // Spark - Deposit USDT
+  allowAction.spark.deposit({ targets: ["USDT"] }),
+  // Spark - Deposit WETH
+  allowAction.spark.deposit({ targets: ["WETH"] }),
+  // Spark - Deposit DSR_sDAI
+  allowAction.spark.deposit({ targets: ["DSR_sDAI"] }),
+  // Spark - Deposit SKY_USDS
+  allowAction.spark.deposit({ targets: ["SKY_USDS"] }),
+  // Spark - stake
+  allowAction.spark.stake(),
+
+  // CowSwap - [AURA, BAL, COMP, COW, CRV, CVX, DAI, ETH, ETHx, GHO, GNO, LDO, OETH, OGN, osETH, rETH, RPL, SAFE, sDAI, SKY, SPK, stETH, sUSDS, SWISE, UNI, USDC, USDM, USDS, USDT, WETH, wstETH] ->
+  // [DAI, ETH, ETHx, GHO, GNO, OETH, osETH, rETH, SAFE, sDAI, stETH, sUSDS, USDC, USDM, USDS, USDT, WETH, wstETH]
   allowAction.cowswap.swap({
-    sell: [SAFE],
-    buy: ["ETH", DAI, stETH, USDC, WETH, wstETH],
-  }),
-  // CowSwap - ETH -> [DAI, osETH, stETH, rETH, USDC, WETH, wstETH]
-  allowAction.cowswap.swap({
-    sell: ["ETH"],
-    buy: [DAI, osETH, stETH, rETH, USDC, WETH, wstETH],
-  }),
-  // CowSwap - [AURA, BAL] -> [ETH, USDC, WETH]
-  allowAction.cowswap.swap({
-    sell: [AURA, BAL],
-    buy: ["ETH", USDC, WETH],
+    sell: [
+      AURA,
+      BAL,
+      COMP,
+      COW,
+      CRV,
+      CVX,
+      DAI,
+      "ETH",
+      ETHx,
+      GHO,
+      GNO,
+      LDO,
+      OETH,
+      OGN,
+      osETH,
+      rETH,
+      RPL,
+      SAFE,
+      sDAI,
+      SKY,
+      // SPK seems that is not deployed yet https://docs.spark.fi/governance/spk-token,
+      stETH,
+      sUSDS,
+      SWISE,
+      UNI,
+      USDC,
+      USDM,
+      USDS,
+      USDT,
+      WETH,
+      wstETH,
+    ],
+    buy: [
+      DAI,
+      "ETH",
+      ETHx,
+      GHO,
+      GNO,
+      rETH,
+      OETH,
+      osETH,
+      SAFE,
+      stETH,
+      sDAI,
+      sUSDS,
+      USDC,
+      USDM,
+      USDS,
+      USDT,
+      WETH,
+      wstETH,
+    ],
   }),
 
   // Lido
   allowAction.lido.deposit(),
 
-  // Uniswap v3 - SAFE + WETH - Current NFT Ids: 711659 and 774338
-  allowAction.uniswap_v3.deposit({ tokens: ["SAFE", "WETH"] }),
+  // Rocket pool
+  allowAction.rocket_pool.deposit(),
+
+  // Stader
+  allowAction.stader.deposit(),
+
+  // StakeWise v3 - Genesis
+  allowAction.stakewise_v3.stake({ targets: ["Genesis"] }),
+  // StakeWise v3 - Chorus one - MEV Max
+  allowAction.stakewise_v3.stake({ targets: ["Chorus One - MEV Max"] }),
+
+  // Uniswap v3 - SAFE + WETH
+  allowAction.uniswap_v3.deposit({
+    tokens: ["SAFE", "WETH"],
+    fees: ["0.3%", "1%"],
+  }),
+  // Uniswap v3 - SAFE + USDC
+  allowAction.uniswap_v3.deposit({
+    tokens: ["SAFE", "USDC"],
+    fees: ["0.3%", "1%"],
+  }),
+  // Uniswap v3 - SAFE + USDT
+  allowAction.uniswap_v3.deposit({
+    tokens: ["SAFE", "USDT"],
+    fees: ["0.3%", "1%"],
+  }),
+  // Uniswap v3 - WETH + USDC
+  allowAction.uniswap_v3.deposit({
+    tokens: ["WETH", "USDC"],
+    fees: ["0.05%", "0.3%"],
+  }),
+  // Uniswap v3 - WETH + USDT
+  allowAction.uniswap_v3.deposit({
+    tokens: ["WETH", "USDT"],
+    fees: ["0.05%", "0.3%"],
+  }),
 ]
