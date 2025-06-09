056705dd4d968eecfb0ba88b71b5e768e0e6f88e
nledo <nicolas@karpatkey.com>
2025-06-09 14:06:33 -0300

    Replace wrong avatar address

--- clients/renaissance-fund/gnosis/instances/main.ts
+++ clients/renaissance-fund/gnosis/instances/main.ts
@@ -4,5 +4,5 @@ export const rolesMod = "0x3C2F6B6c188E1e9474B51e001cF01366eF88727c"
 export const chainId = 100
 
 export const parameters: Parameters = {
-  avatar: "0xdF8EE91120154BdC3Cb628f0535B6511e52327Ff",
+  avatar: "0x25Cb08386BDd1dbf2830a8089f23c967DA244061",
 }
--- clients/renaissance-fund/mainnet/instances/sub.ts
+++ clients/renaissance-fund/mainnet/instances/sub.ts
@@ -4,5 +4,5 @@ export const rolesMod = "0x36603854A10e0201a1129Ae2edCB7c6Fd52AC390"
 export const chainId = 1
 
 export const parameters: Parameters = {
-  avatar: "0xdF8EE91120154BdC3Cb628f0535B6511e52327Ff",
+  avatar: "0x25Cb08386BDd1dbf2830a8089f23c967DA244061",
 }
