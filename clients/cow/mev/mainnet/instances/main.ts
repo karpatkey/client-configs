08b70eea163e310264e48a9532d7b6370d6c06cb
nledo <nicolas@karpatkey.com>
2025-07-11 10:20:03 -0300

    Update instances

--- clients/cow/mev/mainnet/instances/main.ts
+++ clients/cow/mev/mainnet/instances/main.ts
@@ -1,2 +1,9 @@
+import { Parameters } from "../../../parameters"
+
 export const rolesMod = "0x742a6e6F0f7a70AbDe33E50A6186533431410861"
 export const chainId = 1
+
+export const parameters: Parameters = {
+  avatar: "0xCe91228789B57DEb45e66Ca10Ff648385fE7093b",
+}
+
