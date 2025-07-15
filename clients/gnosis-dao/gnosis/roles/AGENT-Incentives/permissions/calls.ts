e36ae02491daaeb584446ae5ac97749dab138379
nledo <nicolas@karpatkey.com>
2025-07-15 16:41:30 -0300

    Gnosis DAO Agent Roles on GC

---
+++ clients/gnosis-dao/gnosis/roles/AGENT-Incentives/permissions/calls.ts
@@ -0,0 +1,15 @@
+import { c } from "zodiac-roles-sdk"
+import { allow } from "zodiac-roles-sdk/kit"
+import { PermissionList } from "@/types"
+import { allowErc20Approve } from "@/helpers"
+import { wstETH } from "@/addresses/gno"
+import { contracts } from "@/contracts"
+import { encodeBytes32String } from "defi-kit"
+
+export default [
+  allowErc20Approve([wstETH], [contracts.gnosis.curve.crvEureUsdGauge]),
+  allow.gnosis.curve.crvEureUsdGauge.deposit_reward_token(
+    wstETH,
+    c.withinAllowance(encodeBytes32String("curve_gauge_amount") as `0x${string}`)
+  )
+] satisfies PermissionList
