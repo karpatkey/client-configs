6c30c6966dcd4a2eff2263cbbf194deaa67d9335
nledo <nicolas@karpatkey.com>
2025-10-17 19:03:16 -0300

    Arbitrum DAO final setup

--- clients/arbitrum-dao/arbitrum/instances/main.ts
+++ clients/arbitrum-dao/arbitrum/instances/main.ts
@@ -1,8 +1,8 @@
 import { Parameters } from "../parameters"
 
-export const rolesMod = "0xBd1099dFD3c11b65FB4BB19A350da2f5B61Efb0d"
+export const rolesMod = "0x80BCb96711067eEa42e0Bc2ea89a545483538Cc8"
 export const chainId = 42161
 
 export const parameters: Parameters = {
-  avatar: "0xC01318baB7ee1f5ba734172bF7718b5DC6Ec90E1",
+  avatar: "0x4D1D9D7741740A3E2ffC5507aC643DbA5e81cAe5",
 }
--- clients/arbitrum-dao/arbitrum/instances/sub.ts
+++ clients/arbitrum-dao/arbitrum/instances/sub.ts
@@ -1,7 +1,7 @@
 import { Parameters } from "../parameters"
 
-export const rolesMod = "0x80BCb96711067eEa42e0Bc2ea89a545483538Cc8"
-export const chainId = 1
+export const rolesMod = "0xB11e6525f40651451c8A95caE7c07E4a1063a4e2"
+export const chainId = 42161
 
 export const parameters: Parameters = {
   avatar: "0x4D1D9D7741740A3E2ffC5507aC643DbA5e81cAe5",
