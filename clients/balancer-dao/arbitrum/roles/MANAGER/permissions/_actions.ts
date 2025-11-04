0b754a7c3a104ae127063e7ca7a3c798df42fc66
nledo <103588782+nledo@users.noreply.github.com>
2025-11-04 11:25:21 -0300

    Balancer Emergency Request (#143)
    * Balancer Emergency Request
    
    * Format code with Prettier
    
    ---------
    
    Co-authored-by: GitHub Actions <actions@github.com>

--- clients/balancer-dao/arbitrum/roles/MANAGER/permissions/_actions.ts
+++ clients/balancer-dao/arbitrum/roles/MANAGER/permissions/_actions.ts
@@ -3,6 +3,9 @@ import { USDC, USDCe } from "@/addresses/arb1"
 import { Parameters } from "../../../parameters"
 
 export default (parameters: Parameters) => [
+  // Balancer v2 - axlBAL/BAL
+  allowAction.balancer_v2.deposit({ targets: ["axlBAL/BAL"] }),
+
   // Circle v2 - Bridge USDC to Ethereum
   allowAction.circle_v2.bridge({
     targets: ["Ethereum"],
