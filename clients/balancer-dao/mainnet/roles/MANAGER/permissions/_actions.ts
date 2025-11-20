8dc6f309d773408825251a549138c88aaaadb6ef
nledo <nicolas@karpatkey.com>
2025-11-20 17:22:40 -0300

    Balancer PUR 8

--- clients/balancer-dao/mainnet/roles/MANAGER/permissions/_actions.ts
+++ clients/balancer-dao/mainnet/roles/MANAGER/permissions/_actions.ts
@@ -23,7 +23,6 @@ import {
   WETH,
   wstETH,
 } from "@/addresses/eth"
-import { kpkGovernance } from "../../../addresses"
 import { Parameters } from "../../../parameters"
 
 export default (parameters: Parameters) => [
@@ -47,13 +46,6 @@ export default (parameters: Parameters) => [
   // Aave v3 Core Market - Borrow GHO
   allowAction.aave_v3.borrow({ market: "Core", targets: ["GHO"] }),
 
-  // Aave - Delegate AAVE and stkAAVE to governance.karpatkey.eth
-  // WARNING!: The delegate action allows delegate() and delegateByType(), the latter is not part of the orginal preset
-  allowAction.aave_v3.delegate({
-    targets: ["AAVE", "stkAAVE"],
-    delegatee: kpkGovernance,
-  }),
-
   // Balancer v2 - 80BAL20wETH/wETH
   allowAction.balancer_v2.deposit({ targets: ["80BAL20wETH/wETH"] }),
   // Balancer v2 - B-80BAL-20WETH
@@ -65,6 +57,15 @@ export default (parameters: Parameters) => [
     sender: parameters.avatar,
     recipient: parameters.avatar,
   }),
+
+  // Compound v3 - Deposit USDC
+  allowAction.compound_v3.deposit({ targets: ["cUSDCv3"], tokens: ["USDC"] }),
+  // Compound v3 - Deposit USDS
+  allowAction.compound_v3.deposit({ targets: ["cUSDSv3"], tokens: ["USDS"] }),
+  // Compound v3 - Deposit USDT
+  allowAction.compound_v3.deposit({ targets: ["cUSDTv3"], tokens: ["USDT"] }),
+  // Compound v3 - Deposit WETH
+  allowAction.compound_v3.deposit({ targets: ["cWETHv3"], tokens: ["WETH"] }),
 
   // Convex - ETH/OETH
   allowAction.convex.deposit({ targets: ["174"] }),
@@ -135,13 +136,34 @@ export default (parameters: Parameters) => [
   // Lido
   allowAction.lido.deposit(),
 
+  // Morpho Vault - kpk ETH v2
+  allowAction.morphoVaults.deposit({
+    targets: ["0xBb50A5341368751024ddf33385BA8cf61fE65FF9"],
+  }),
+  // Morpho Vault - kpk EURC v2
+  allowAction.morphoVaults.deposit({
+    targets: ["0xa877D5bb0274dcCbA8556154A30E1Ca4021a275f"],
+  }),
+  // Morpho Vault - kpk USDC v2
+  allowAction.morphoVaults.deposit({
+    targets: ["0x4Ef53d2cAa51C447fdFEEedee8F07FD1962C9ee6"],
+  }),
+
   // Rocket Pool
   allowAction.rocket_pool.deposit(),
 
+  // Spark - Deposit USDC
+  allowAction.spark.deposit({ targets: ["USDC"] }),
+  // Spark - Deposit USDT
+  allowAction.spark.deposit({ targets: ["USDT"] }),
   // Spark - DSR_sDAI
   allowAction.spark.deposit({ targets: ["DSR_sDAI"] }),
-  // Spark - SKY_USDS
+  // Spark - SKY_spUSDC
+  allowAction.spark.deposit({ targets: ["SKY_spUSDC"] }),
+  // Spark - SKY_sUSDS
   allowAction.spark.deposit({ targets: ["SKY_sUSDS"] }),
+  // Spark - SKY_spUSDT
+  allowAction.spark.deposit({ targets: ["SKY_spUSDT"] }),
 
   // StakeWise v3 - Genesis Vault
   allowAction.stakewise_v3.stake({ targets: ["Genesis"] }),
