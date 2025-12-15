c9119f9ef364306c7a5013dc867ff712d9370146
nledo <nicolas@karpatkey.com>
2025-12-15 15:50:35 -0300

    Aleph Setup

---
+++ clients/aleph/mainnet/instances/main.ts
@@ -0,0 +1,8 @@
+import { Parameters } from "../parameters"
+
+export const rolesMod = ""
+export const chainId = 1
+
+export const parameters: Parameters = {
+  avatar: "0xC01318baB7ee1f5ba734172bF7718b5DC6Ec90E1",
+}
