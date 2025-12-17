c3cfd5f1920df1a834a19e75d0b1c0fa1179408d
nledo <nicolas@karpatkey.com>
2025-12-17 10:57:54 -0300

    Avatar Safe update

--- clients/aleph/mainnet/instances/main.ts
+++ clients/aleph/mainnet/instances/main.ts
@@ -4,5 +4,5 @@ export const rolesMod = "0xCEE06943E829367ff1Bd6B4670cb4a1148636d26"
 export const chainId = 1
 
 export const parameters: Parameters = {
-  avatar: "0x28DA1782Aad1d86c66EdcCf81b58486234280bcD",
+  avatar: "0xdE53bCdA6527D2aa15Ec733352C8d393C2E0eC99",
 }
