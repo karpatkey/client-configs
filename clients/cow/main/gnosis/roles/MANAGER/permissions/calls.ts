1e69114f69b91abe21b8961c18bdf027e4b2d0cd
nledo <nicolas@karpatkey.com>
2025-12-23 16:59:13 -0300

    Updates on Gnosis Chain

--- clients/cow/main/gnosis/roles/MANAGER/permissions/calls.ts
+++ clients/cow/main/gnosis/roles/MANAGER/permissions/calls.ts
@@ -2,7 +2,16 @@ import { allow } from "zodiac-roles-sdk/kit"
 import { PermissionList } from "@/types"
 import { c } from "zodiac-roles-sdk"
 import { contracts } from "@/contracts"
-import { WETH } from "@/addresses/gno"
+import { 
+  EURe,
+  USDC,
+  USDCe,
+  USDT,
+  WETH,
+  WXDAI,
+  x3CRV,
+  curve,
+} from "@/addresses/gno"
 import { allowErc20Approve } from "@/helpers"
 import { Parameters } from "../../../../../parameters"
 
@@ -11,6 +20,40 @@ export default (parameters: Parameters) =>
     // Wrapping and unwrapping of XDAI, WXDAI
     allow.gnosis.wxdai.deposit({ send: true }),
     allow.gnosis.wxdai.withdraw(),
+
+    // Curve - EURe/x3CRV
+    allowErc20Approve([EURe, x3CRV], [contracts.gnosis.curve.crvEureUsdPool]),
+    allow.gnosis.curve.crvEureUsdPool["add_liquidity(uint256[2],uint256)"](),
+    allow.gnosis.curve.crvEureUsdPool["remove_liquidity(uint256,uint256[2])"](),
+    allow.gnosis.curve.crvEureUsdPool[
+      "remove_liquidity_one_coin(uint256,uint256,uint256)"
+    ](),
+    allowErc20Approve(
+      [EURe, USDC, USDT, WXDAI],
+      [contracts.gnosis.curve.crvEureUsdZap]
+    ),
+    allow.gnosis.curve.crvEureUsdZap["add_liquidity(uint256[4],uint256)"](),
+    allow.gnosis.curve.crvEureUsdZap["remove_liquidity(uint256,uint256[4])"](),
+    allow.gnosis.curve.crvEureUsdZap[
+      "remove_liquidity_one_coin(uint256,uint256,uint256)"
+    ](),
+    allowErc20Approve(
+      [curve.crvEureUsd],
+      [contracts.gnosis.curve.crvEureUsdGauge]
+    ),
+    allow.gnosis.curve.crvEureUsdGauge["deposit(uint256)"](),
+    allow.gnosis.curve.crvEureUsdGauge["withdraw(uint256)"](),
+    allow.gnosis.curve.crvEureUsdGauge["claim_rewards()"](),
+  
+    /*********************************************
+     * Swaps
+     *********************************************/
+    // Swap USDC.e -> USDC
+    allowErc20Approve([USDCe], [contracts.gnosis.gnosisBridge.usdcTransmuter]),
+    allow.gnosis.gnosisBridge.usdcTransmuter.withdraw(),
+    // Swap USDC -> USDC.e
+    allowErc20Approve([USDC], [contracts.gnosis.gnosisBridge.usdcTransmuter]),
+    allow.gnosis.gnosisBridge.usdcTransmuter.deposit(),
 
     /*********************************************
      * Bridge
