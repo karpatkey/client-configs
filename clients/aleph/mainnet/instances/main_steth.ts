d6ab25b8b6dfbc92d187a76c42f34ac6b406de0e
nledo <103588782+nledo@users.noreply.github.com>
2026-02-13 15:27:36 -0300

    Aleph stETH Fund Setup (#173)

---
+++ clients/aleph/mainnet/instances/main_steth.ts
@@ -0,0 +1,8 @@
+import { Parameters } from "../parameters"
+
+export const rolesMod = "0xDC6B02cd410496328B0d5107B41E9acF103ec1e8"
+export const chainId = 1
+
+export const parameters: Parameters = {
+  avatar: "0xEec0048ef14b44B1DBEc1Cf0f7c9c9E31F3d42CA",
+}
---
+++ clients/aleph/mainnet/instances/sub_steth.ts
@@ -0,0 +1,8 @@
+import { Parameters } from "../parameters"
+
+export const rolesMod = "0x3F2DA45eD83e60c8BE9ecd2DC1e44c1421D4D9cC"
+export const chainId = 1
+
+export const parameters: Parameters = {
+  avatar: "0xEec0048ef14b44B1DBEc1Cf0f7c9c9E31F3d42CA",
+}
