d1fd8fb0aa13d5ea50e78bc2e4e0d38c25815546
nledo <nicolas@karpatkey.com>
2026-03-09 15:20:45 -0300

    Aleph osETH mandate setup

---
+++ clients/aleph/mainnet/instances/sub_oseth.ts
@@ -0,0 +1,8 @@
+import { Parameters } from "../parameters"
+
+export const rolesMod = "0x231fC171c627DD91b1e760d11a5E3Ad0D28a4dB0"
+export const chainId = 1
+
+export const parameters: Parameters = {
+  avatar: "0xE3a94987E8958A48bE74E5C4b0E68D7dcdCeE2A7",
+}
