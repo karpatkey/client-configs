2f4aaad832083254169526aa6d3e1f352e392de9
nledo <nicolas@karpatkey.com>
2026-03-09 10:04:25 -0300

    CoW Policies Updates Iteration #1

--- clients/cow/main/gnosis/roles/MANAGER/permissions/_actions.ts
+++ clients/cow/main/gnosis/roles/MANAGER/permissions/_actions.ts
@@ -13,12 +13,12 @@ import {
 } from "@/addresses/gno"
 
 export default [
-  // Aave v3 - Deposit EURe
-  allowAction.aave_v3.deposit({ targets: ["EURe"] }),
-  // Aave v3 - Deposit sDAI
-  allowAction.aave_v3.deposit({ targets: ["sDAI"] }),
-  // Aave v3 - Deposit XDAI
-  allowAction.aave_v3.deposit({ targets: ["XDAI"] }),
+  // // Aave v3 - Deposit EURe
+  // allowAction.aave_v3.deposit({ targets: ["EURe"] }),
+  // // Aave v3 - Deposit sDAI
+  // allowAction.aave_v3.deposit({ targets: ["sDAI"] }),
+  // // Aave v3 - Deposit XDAI
+  // allowAction.aave_v3.deposit({ targets: ["XDAI"] }),
 
   // CowSwap - [COW, EURe, GNO, sDAI, USDC, USDC.e, USDT, WETH, wstETH, WXDAI, XDAI] <->
   // [COW, EURe, GNO, sDAI, USDC, USDC.e, USDT, WETH, wstETH, WXDAI, XDAI]
@@ -39,10 +39,10 @@ export default [
     buy: ["XDAI", COW, EURe, GNO, sDAI, USDC, USDCe, USDT, WETH, wstETH, WXDAI],
   }),
 
-  // Spark - Deposit EURe
-  allowAction.spark.deposit({ targets: ["EURe"] }),
-  // Spark - Deposit USDC.e
-  allowAction.spark.deposit({ targets: ["USDC.e"] }),
-  // Spark - DSR_sDAI
-  allowAction.spark.deposit({ targets: ["DSR_sDAI"] }),
+  // // Spark - Deposit EURe
+  // allowAction.spark.deposit({ targets: ["EURe"] }),
+  // // Spark - Deposit USDC.e
+  // allowAction.spark.deposit({ targets: ["USDC.e"] }),
+  // // Spark - DSR_sDAI
+  // allowAction.spark.deposit({ targets: ["DSR_sDAI"] }),
 ]
--- clients/cow/main/gnosis/roles/MANAGER/permissions/calls.ts
+++ clients/cow/main/gnosis/roles/MANAGER/permissions/calls.ts
@@ -24,126 +24,126 @@ export default (parameters: Parameters) =>
     allow.gnosis.wxdai.deposit({ send: true }),
     allow.gnosis.wxdai.withdraw(),
 
-    // Curve - EURe/x3CRV
-    allowErc20Approve([EURe, x3CRV], [contracts.gnosis.curve.crvEureUsdPool]),
-    allow.gnosis.curve.crvEureUsdPool["add_liquidity(uint256[2],uint256)"](),
-    allow.gnosis.curve.crvEureUsdPool["remove_liquidity(uint256,uint256[2])"](),
-    allow.gnosis.curve.crvEureUsdPool[
-      "remove_liquidity_one_coin(uint256,uint256,uint256)"
-    ](),
-    allowErc20Approve(
-      [EURe, USDC, USDT, WXDAI],
-      [contracts.gnosis.curve.crvEureUsdZap]
-    ),
-    allow.gnosis.curve.crvEureUsdZap["add_liquidity(uint256[4],uint256)"](),
-    allow.gnosis.curve.crvEureUsdZap["remove_liquidity(uint256,uint256[4])"](),
-    allow.gnosis.curve.crvEureUsdZap[
-      "remove_liquidity_one_coin(uint256,uint256,uint256)"
-    ](),
-    allowErc20Approve(
-      [curve.crvEureUsd],
-      [contracts.gnosis.curve.crvEureUsdGauge]
-    ),
-    allow.gnosis.curve.crvEureUsdGauge["deposit(uint256)"](),
-    allow.gnosis.curve.crvEureUsdGauge["withdraw(uint256)"](),
-    allow.gnosis.curve.crvEureUsdGauge["claim_rewards()"](),
+    // // Curve - EURe/x3CRV
+    // allowErc20Approve([EURe, x3CRV], [contracts.gnosis.curve.crvEureUsdPool]),
+    // allow.gnosis.curve.crvEureUsdPool["add_liquidity(uint256[2],uint256)"](),
+    // allow.gnosis.curve.crvEureUsdPool["remove_liquidity(uint256,uint256[2])"](),
+    // allow.gnosis.curve.crvEureUsdPool[
+    //   "remove_liquidity_one_coin(uint256,uint256,uint256)"
+    // ](),
+    // allowErc20Approve(
+    //   [EURe, USDC, USDT, WXDAI],
+    //   [contracts.gnosis.curve.crvEureUsdZap]
+    // ),
+    // allow.gnosis.curve.crvEureUsdZap["add_liquidity(uint256[4],uint256)"](),
+    // allow.gnosis.curve.crvEureUsdZap["remove_liquidity(uint256,uint256[4])"](),
+    // allow.gnosis.curve.crvEureUsdZap[
+    //   "remove_liquidity_one_coin(uint256,uint256,uint256)"
+    // ](),
+    // allowErc20Approve(
+    //   [curve.crvEureUsd],
+    //   [contracts.gnosis.curve.crvEureUsdGauge]
+    // ),
+    // allow.gnosis.curve.crvEureUsdGauge["deposit(uint256)"](),
+    // allow.gnosis.curve.crvEureUsdGauge["withdraw(uint256)"](),
+    // allow.gnosis.curve.crvEureUsdGauge["claim_rewards()"](),
 
-    /*********************************************
-     * Swaps
-     *********************************************/
-    // Swap USDC.e -> USDC
-    allowErc20Approve([USDCe], [contracts.gnosis.gnosisBridge.usdcTransmuter]),
-    allow.gnosis.gnosisBridge.usdcTransmuter.withdraw(),
-    // Swap USDC -> USDC.e
-    allowErc20Approve([USDC], [contracts.gnosis.gnosisBridge.usdcTransmuter]),
-    allow.gnosis.gnosisBridge.usdcTransmuter.deposit(),
+    // /*********************************************
+    //  * Swaps
+    //  *********************************************/
+    // // Swap USDC.e -> USDC
+    // allowErc20Approve([USDCe], [contracts.gnosis.gnosisBridge.usdcTransmuter]),
+    // allow.gnosis.gnosisBridge.usdcTransmuter.withdraw(),
+    // // Swap USDC -> USDC.e
+    // allowErc20Approve([USDC], [contracts.gnosis.gnosisBridge.usdcTransmuter]),
+    // allow.gnosis.gnosisBridge.usdcTransmuter.deposit(),
 
-    /*********************************************
-     * Transfers
-     *********************************************/
-    // Transfer XDAI to CoW Defense Fund
-    allowEthTransfer(legalDefenseFund),
+    // /*********************************************
+    //  * Transfers
+    //  *********************************************/
+    // // Transfer XDAI to CoW Defense Fund
+    // allowEthTransfer(legalDefenseFund),
 
-    /*********************************************
-     * Bridge
-     *********************************************/
-    // Gnosis -> Mainnet
-    // GNO - Gnosis Bridge
-    allow.gnosis.gno.transferAndCall(
-      contracts.gnosis.gnosisBridge.xdaiBridge,
-      undefined,
-      parameters.avatar
-    ),
+    // /*********************************************
+    //  * Bridge
+    //  *********************************************/
+    // // Gnosis -> Mainnet
+    // // GNO - Gnosis Bridge
+    // allow.gnosis.gno.transferAndCall(
+    //   contracts.gnosis.gnosisBridge.xdaiBridge,
+    //   undefined,
+    //   parameters.avatar
+    // ),
 
-    // USDC - Gnosis Bridge
-    allow.gnosis.usdc.transferAndCall(
-      contracts.gnosis.gnosisBridge.xdaiBridge,
-      undefined,
-      parameters.avatar
-    ),
+    // // USDC - Gnosis Bridge
+    // allow.gnosis.usdc.transferAndCall(
+    //   contracts.gnosis.gnosisBridge.xdaiBridge,
+    //   undefined,
+    //   parameters.avatar
+    // ),
 
-    // USDT - Gnosis Bridge
-    {
-      ...allow.gnosis.gno.transferAndCall(
-        contracts.gnosis.gnosisBridge.xdaiBridge,
-        undefined,
-        parameters.avatar
-      ),
-      targetAddress: USDT,
-    },
+    // // USDT - Gnosis Bridge
+    // {
+    //   ...allow.gnosis.gno.transferAndCall(
+    //     contracts.gnosis.gnosisBridge.xdaiBridge,
+    //     undefined,
+    //     parameters.avatar
+    //   ),
+    //   targetAddress: USDT,
+    // },
 
-    // WBTC - Gnosis Bridge
-    {
-      ...allow.gnosis.gno.transferAndCall(
-        contracts.gnosis.gnosisBridge.xdaiBridge,
-        undefined,
-        parameters.avatar
-      ),
-      targetAddress: WBTC,
-    },
+    // // WBTC - Gnosis Bridge
+    // {
+    //   ...allow.gnosis.gno.transferAndCall(
+    //     contracts.gnosis.gnosisBridge.xdaiBridge,
+    //     undefined,
+    //     parameters.avatar
+    //   ),
+    //   targetAddress: WBTC,
+    // },
 
-    // WETH - Gnosis Bridge
-    {
-      ...allow.gnosis.gno.transferAndCall(
-        contracts.gnosis.gnosisBridge.xdaiBridge,
-        undefined,
-        parameters.avatar
-      ),
-      targetAddress: WETH,
-    },
+    // // WETH - Gnosis Bridge
+    // {
+    //   ...allow.gnosis.gno.transferAndCall(
+    //     contracts.gnosis.gnosisBridge.xdaiBridge,
+    //     undefined,
+    //     parameters.avatar
+    //   ),
+    //   targetAddress: WETH,
+    // },
 
-    // WETH -> ETH - Stargate
-    allowErc20Approve([WETH], [contracts.gnosis.stargate.poolNative]),
-    allow.gnosis.stargate.poolNative.send(
-      {
-        dstEid: "30101", // Ethereum
-        to: "0x" + parameters.avatar.slice(2).padStart(64, "0"),
-        extraOptions: "0x",
-        composeMsg: "0x",
-        oftCmd: "0x",
-      },
-      undefined,
-      c.avatar,
-      {
-        send: true,
-      }
-    ),
+    // // WETH -> ETH - Stargate
+    // allowErc20Approve([WETH], [contracts.gnosis.stargate.poolNative]),
+    // allow.gnosis.stargate.poolNative.send(
+    //   {
+    //     dstEid: "30101", // Ethereum
+    //     to: "0x" + parameters.avatar.slice(2).padStart(64, "0"),
+    //     extraOptions: "0x",
+    //     composeMsg: "0x",
+    //     oftCmd: "0x",
+    //   },
+    //   undefined,
+    //   c.avatar,
+    //   {
+    //     send: true,
+    //   }
+    // ),
 
-    // wstETH - Gnosis Bridge
-    {
-      ...allow.gnosis.gno.transferAndCall(
-        contracts.gnosis.gnosisBridge.xdaiBridge,
-        undefined,
-        parameters.avatar
-      ),
-      targetAddress: wstETH,
-    },
+    // // wstETH - Gnosis Bridge
+    // {
+    //   ...allow.gnosis.gno.transferAndCall(
+    //     contracts.gnosis.gnosisBridge.xdaiBridge,
+    //     undefined,
+    //     parameters.avatar
+    //   ),
+    //   targetAddress: wstETH,
+    // },
 
-    // XDAI -> DAI - Gnosis Bridge
-    allow.gnosis.gnosisBridge.xdaiBridge2.relayTokens(c.avatar, {
-      send: true,
-    }),
+    // // XDAI -> DAI - Gnosis Bridge
+    // allow.gnosis.gnosisBridge.xdaiBridge2.relayTokens(c.avatar, {
+    //   send: true,
+    // }),
 
-    // XDAI -> USDS - Gnosis Bridge
-    allow.gnosis.gnosisBridge.usdsDeposit.relayTokens(c.avatar, { send: true }),
+    // // XDAI -> USDS - Gnosis Bridge
+    // allow.gnosis.gnosisBridge.usdsDeposit.relayTokens(c.avatar, { send: true }),
   ] satisfies PermissionList
