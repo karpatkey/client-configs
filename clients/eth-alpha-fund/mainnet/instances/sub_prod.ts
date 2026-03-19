1a806943e59869c3b2574951ad6ce9d5724a25b2
nledo <nicolas@karpatkey.com>
2026-03-19 13:57:14 -0300

    ETH Fund Agents and Manager Safe Policies

---
+++ clients/eth-alpha-fund/mainnet/instances/sub_prod.ts
@@ -0,0 +1,9 @@
+import { Parameters } from "../parameters"
+
+export const rolesMod = "0x97Ab9e7a2275d4bD86913809b3c29C4dDA49c694"
+export const chainId = 1
+
+export const parameters: Parameters = {
+  avatar: "0x99b9F5F24205Cb88E33b1CC72008f644Fc23768b",
+  shares: "0xED01a1Fe4e020Ca901F43E17B6203A7e0cEa818C",
+}
