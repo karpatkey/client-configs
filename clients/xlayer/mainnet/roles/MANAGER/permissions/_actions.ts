d6040531870f76e0a5cb26c91045d4a3658dd371
nledo <nicolas@karpatkey.com>
2026-03-27 15:15:14 -0300

    Fix

--- clients/xlayer/mainnet/roles/MANAGER/permissions/_actions.ts
+++ clients/xlayer/mainnet/roles/MANAGER/permissions/_actions.ts
@@ -1,1 +1,10 @@
-export default []
+import { allow as allowAction } from "defi-kit/eth"
+import { morpho } from "@/addresses/eth"
+
+export default [
+  // Morpho Vault - kpk USDT Prime v2
+  allowAction.morphoVaults.deposit({
+    targets: [morpho.kpkUsdtPrimeV2],
+  }),
+]
+
--- clients/xlayer/xlayer/roles/MANAGER/permissions/_actions.ts
+++ clients/xlayer/xlayer/roles/MANAGER/permissions/_actions.ts
@@ -1,9 +1,1 @@
-import { allow as allowAction } from "defi-kit/eth"
-import { morpho } from "@/addresses/eth"
-
-export default [
-  // Morpho Vault - kpk USDT Prime v2
-  allowAction.morphoVaults.deposit({
-    targets: [morpho.kpkUsdtPrimeV2],
-  }),
-]
+export default []
