a38379eb145204de8ed03385e3b8dd3fb1b028e3
nledo <nicolas@karpatkey.com>
2026-04-07 16:36:53 -0300

    Fix REQUESTS_AGENTS policy

--- clients/usd-alpha-fund/mainnet/roles/REQUESTS_AGENTS/permissions/calls.ts
+++ clients/usd-alpha-fund/mainnet/roles/REQUESTS_AGENTS/permissions/calls.ts
@@ -1,12 +1,16 @@
 import { allow } from "zodiac-roles-sdk/kit"
-import { WETH } from "@/addresses/eth"
+import { USDC, USDT } from "@/addresses/eth"
 import { PermissionList } from "@/types"
 import { Parameters } from "../../../parameters"
 
 export default (parameters: Parameters) =>
   [
     {
-      ...allow.mainnet.oiv.shares.processRequests(undefined, undefined, WETH),
+      ...allow.mainnet.oiv.shares.processRequests(undefined, undefined, USDC),
+      targetAddress: parameters.shares,
+    },
+    {
+      ...allow.mainnet.oiv.shares.processRequests(undefined, undefined, USDT),
       targetAddress: parameters.shares,
     },
   ] satisfies PermissionList
