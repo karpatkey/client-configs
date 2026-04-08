cb7d43c5293ec576cd38d4c0baaf0728e90c0144
nledo <nicolas@karpatkey.com>
2026-04-08 09:45:19 -0300

    Fix REQUESTS_AGENTS policy

--- clients/eth-alpha-fund/mainnet/roles/REQUESTS_AGENTS/permissions/calls.ts
+++ clients/eth-alpha-fund/mainnet/roles/REQUESTS_AGENTS/permissions/calls.ts
@@ -1,12 +1,1 @@
-import { allow } from "zodiac-roles-sdk/kit"
-import { WETH } from "@/addresses/eth"
-import { PermissionList } from "@/types"
-import { Parameters } from "../../../parameters"
-
-export default (parameters: Parameters) =>
-  [
-    {
-      ...allow.mainnet.oiv.shares.processRequests(undefined, undefined, WETH),
-      targetAddress: parameters.shares,
-    },
-  ] satisfies PermissionList
+export default []
--- clients/usd-alpha-fund/mainnet/roles/REQUESTS_AGENTS/permissions/calls.ts
+++ clients/usd-alpha-fund/mainnet/roles/REQUESTS_AGENTS/permissions/calls.ts
@@ -1,16 +1,1 @@
-import { allow } from "zodiac-roles-sdk/kit"
-import { USDC, USDT } from "@/addresses/eth"
-import { PermissionList } from "@/types"
-import { Parameters } from "../../../parameters"
-
-export default (parameters: Parameters) =>
-  [
-    {
-      ...allow.mainnet.oiv.shares.processRequests(undefined, undefined, USDC),
-      targetAddress: parameters.shares,
-    },
-    {
-      ...allow.mainnet.oiv.shares.processRequests(undefined, undefined, USDT),
-      targetAddress: parameters.shares,
-    },
-  ] satisfies PermissionList
+export default []
