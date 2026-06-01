b3e68be5a4f30fb2e03e180bbcea7539ee93dcee
nledo <nicolas@karpatkey.com>
2026-06-01 16:06:24 -0300

    Nexus PUR6 fixes

--- clients/nexus-mutual/mainnet/roles/MANAGER/permissions/calls.ts
+++ clients/nexus-mutual/mainnet/roles/MANAGER/permissions/calls.ts
@@ -469,8 +469,8 @@ export default (parameters: Parameters) =>
 
     // Nexus Mutual - RWIV Vault interaction
     allowErc20Approve([USDC], [contracts.mainnet.nexus.rwiv]),
-    allow.mainnet.nexus.rwiv.deposit(undefined, c.avatar),
-    allow.mainnet.nexus.rwiv.withdraw(undefined, c.avatar, c.avatar),
+    allow.mainnet.nexus.rwiv.requestDeposit(undefined, c.avatar, c.avatar),
+    allow.mainnet.nexus.rwiv.requestRedeem(undefined, c.avatar, c.avatar),
 
     // Sky - DSR (DAI Savings Rate)
     // The DsrManager provides an easy to use smart contract that allows
